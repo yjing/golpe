3306c18b775a6c867fa3f4114df083482077c455
Stefano Susini <s.susini@gmail.com>
2013-04-25 18:52:25 +0100

    Set up App Configuration

---
+++ app/Controller/ActivityLogsController.php
@@ -0,0 +1,133 @@
+<?php
+
+App::import('Controller', 'REST');
+
+class ActivityLogsController extends RESTController {
+
+    public $uses = array('ActivityLog', 'User');
+    public $components = array('Session', 'RequestHandler');
+    
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+
+    public function index() {
+        parent::index();
+        
+//        $user = $this->Session->read("Auth.User");
+        $user = $this->Auth->user();
+        
+        $mode = "";
+        if(isset($this->request->query['mode'])) {
+            $mode = $this->request->query['mode'];
+        }
+        if(!in_array($mode, array("mine", "team", "public"))) {
+            $mode = "mine";
+        }
+        
+        $conditions = array();
+        switch ($mode) {
+            case "team":
+                $conditions["ActivityLog.user_id"] = $this->User->getTeamComponentsId($user['id']);
+                break;
+            case "public":
+                $conditions["ActivityLog.visibility_level"] = "PUBLIC";
+                break;
+            default:
+                $conditions["ActivityLog.user_id"] = $user['id'];
+                break;
+        }
+        
+        $this->ActivityLog->contain(array(
+            "Media" => array("id", "user_id", "visibility_level"),
+            "Comment" => array("id", "user_id", "visibility_level"),
+            "User" => array("id", "username", "role")
+        ));
+        
+        $this->_setResponseJSON($this->ActivityLog->find('all',
+            array(
+                'conditions' => $conditions,
+                'recursive' => 1
+            )
+        ));
+    }
+
+    public function view($id = null) {
+        parent::view($id);
+        
+        $user = $this->Session->read("Auth.User");
+        
+        $this->ActivityLog->contain(array(
+            "Media" => array(
+                "id", 
+                "filename", 
+                "content-type", 
+                "content-size", 
+                "status",
+                "visibility_level",
+                "created", 
+                "modified",
+                "meta",
+                "has_thumb",
+                "User" => array("id", "username")
+            ),
+            "Comment" => array(
+                "id", 
+                "content",
+                "visibility_level", 
+                "created", 
+                "modified",
+                "User" => array("id", "username"),
+                "Media" => array(
+                    "id", 
+                    "filename", 
+                    "content-type", 
+                    "content-size", 
+                    "status",
+                    "visibility_level",
+                    "created", 
+                    "modified",
+                    "meta",
+                    "User" => array("id", "username")
+                )
+            ),
+            "User" => array("id", "username", "role", "created", "modified")
+        ));
+        
+        $this->_setResponseJSON($this->ActivityLog->find('first',
+            array(
+                'conditions' => array(
+                    'ActivityLog.id' => $id
+                ),
+                'recursive' => 2
+            )
+        ));
+    }
+
+    public function add() {
+        parent::add();
+        
+        $saved = $this->ActivityLog->save($this->request->data);
+        $this->_setResponseJSON($saved);
+    }
+
+    public function update($id = null) {
+        parent::update($id);
+        
+        $this->ActivityLog->id = $id;
+        $this->_setResponseJSON($this->ActivityLog->save($this->data));
+        
+    }
+
+    public function delete($id = null) {
+        parent::delete($id);
+        
+        $deleted = $this->ActivityLog->delete($id);
+        
+        $this->_setResponseJSON(array('deleted'=>$deleted));
+        
+    }
+    
+}
+
+?>
---
+++ app/Model/ActivityLog.php
@@ -0,0 +1,46 @@
+<?php
+
+App::import('Model', 'Media');
+App::import('Model', 'ActivityLogMedia');
+App::import('Component', 'Session');
+
+// app/Model/User.php
+class ActivityLog extends UserAwareModel {
+
+    public $name = 'ActivityLog';
+    public $useTable = "activity_logs";
+    public $actsAs = array('Containable', 'HasMedia');
+
+    public $belongsTo = "User";
+    public $hasAndBelongsToMany = array(
+        "Media" => array(
+            'order' => 'Media.modified DESC'
+        ) 
+        ,"Comment" => array(
+            'order' => 'Comment.modified DESC'
+        )
+    );
+    
+    public function beforeSave($options = array()) {
+        parent::beforeSave($options);
+        
+        if($this->data[$this->alias]) {
+            
+            $this->Session = new SessionComponent(new ComponentCollection());
+            $user = $this->Session->read("Auth.User");
+
+            $this->data[$this->alias]['user_id'] = $user['id'];
+            $this->data[$this->alias]['draft'] = false;
+            
+            return true;
+            
+        } else {
+            
+            return false;
+            
+        }
+    }
+    
+}
+
+?>
