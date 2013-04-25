14d6ff260a15f6c7b6305a5fb0ee15799d288a1d
stefanosusini <root@ssazuretest.cloudapp.net>
2013-04-25 14:25:48 +0000

    Added UsersManager Plugin

---
+++ app/Plugin/UsersManager/Controller/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('UsersManagerAppController', 'UsersManager.Controller');
+/**
+ * Users Controller
+ *
+ */
+class UsersController extends UsersManagerAppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ app/Plugin/UsersManager/Test/Case/Controller/UsersControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('UsersController', 'UsersManager.Controller');
+
+/**
+ * UsersController Test Case
+ *
+ */
+class UsersControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.users_manager.user'
+	);
+
+}
