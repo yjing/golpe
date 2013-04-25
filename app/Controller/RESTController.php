2aeeed1322de48157de06421bb6e5465eb8a1d9b
Stefano Susini <s.susini@gmail.com>
2013-04-25 19:42:20 +0100

    Set up App Configuration

--- app/Controller/RESTController.php
+++ app/Controller/RESTController.php
@@ -60,6 +60,7 @@ abstract class RESTController extends AppController {
                 $action_auth = $this->_authorization[$this->action];
                 if(is_array($action_auth)) {
                     $ret = in_array($user['role'], array_keys($action_auth));
+                    debug($ret);
                 } else {
                     $ret = $action_auth;
                 }
