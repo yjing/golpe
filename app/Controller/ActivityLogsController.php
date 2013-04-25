9ca7ead63dbf5f65479144c4d3000c99376b77a7
Stefano Susini <s.susini@gmail.com>
2013-04-25 19:19:54 +0100

    Set up App Configuration

--- app/Controller/ActivityLogsController.php
+++ app/Controller/ActivityLogsController.php
@@ -13,6 +13,10 @@ class ActivityLogsController extends RESTController {
 
     public function index() {
         parent::index();
+    }
+    
+    public function index2() {
+        parent::index();
         
 //        $user = $this->Session->read("Auth.User");
         $user = $this->Auth->user();
