1f44f0bd9e640f74c8b9c545a58f22eb77343629
Stefano Susini <s.susini@gmail.com>
2013-04-25 15:51:59 +0100

    Added RESTController and UsersController

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -109,6 +109,20 @@ CakeLog::config('error', array(
 ));
 
 /**
+* REST Configuration
+*/
+Router::resourceMap(array(
+    array('action' => 'index', 'method' => 'GET', 'id' => false),
+    array('action' => 'view', 'method' => 'GET', 'id' => true),
+    array('action' => 'add', 'method' => 'POST', 'id' => false),
+    array('action' => 'edit', 'method' => 'PUT', 'id' => true),
+    array('action' => 'delete', 'method' => 'DELETE', 'id' => true),
+    array('action' => 'update', 'method' => 'POST', 'id' => true)
+));
+
+Router::mapResources(array('users'));
+        
+/**
  * Configures JSON Exceptions Handler 
  */
 App::uses('AppJSONExceptionHandler', 'Lib');
