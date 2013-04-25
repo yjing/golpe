3ceaa38a10a14ea5274010ebc09c8174d63b4ff8
Stefano Susini <s.susini@gmail.com>
2013-04-25 15:49:49 +0100

    Added RESTController and UsersController

---
+++ app/Controller/RESTController.php
@@ -0,0 +1,137 @@
+<?php
+
+abstract class RESTController extends AppController {
+
+    public $components = array('RequestHandler');
+    
+    public function beforeFilter() {
+//        parent::beforeFilter();
+        $this->autoRender = false;
+        $this->RequestHandler->renderAs($this, 'json');
+        
+        // In a REST service, if unauthorized, I just need to send a 501 status code to the client
+//        $this->Auth->autoRedirect = false;
+//        if (!$this->Auth->loggedIn() && !in_array($this->action, $this->Auth->allowedActions)) {
+//            throw new UnauthorizedException();
+//        }
+    }
+
+    public function index() {
+      
+        if (!$this->request->is('get')) {
+            
+            $this->_ReportMethodNotAllowed("GET", 'index');
+            
+        }
+        
+    }
+
+    public function view($id = null) {
+      
+        if (!$this->request->is('get')) {
+            
+            $this->_ReportMethodNotAllowed("GET", 'view');
+            
+        }
+        
+    }
+
+    public function add() {
+      
+        if (!$this->request->is('post')) {
+            
+            $this->_ReportMethodNotAllowed("POST", 'add');
+            
+        }
+        
+    }
+
+    public function update($id = null) {
+        
+        if (!$this->request->is('post')) {
+            
+            $this->_ReportMethodNotAllowed("POST", 'update');
+            
+        }
+        
+    }
+
+    public function edit($id = null) {
+        
+        if (!$this->request->is('put')) {
+            
+            $this->_ReportMethodNotAllowed("PUT", 'edit');
+            
+        }
+        
+    }
+
+    public function delete($id = null) {
+        
+        if (!$this->request->is('delete')) {
+            
+            $this->_ReportMethodNotAllowed("DELETE", 'delete');
+            
+        }
+        
+    }
+    
+    
+    /**
+     * RESPONSE HELPERS: 
+     */
+    
+    protected function _setResponseJSON($content) {
+        $this->set(array(
+            'content' => $content,
+            '_serialize' => 'content'
+        ));
+    }
+
+
+    
+    /**
+     * ERROR HANDLERS HELPERS:
+     */    
+    protected function _ReportValidationErrors($errors){
+        $message = array(
+            'errors' => $errors,
+            'status' => 400
+        );
+        throw new BadRequestException(json_encode($message));
+    }
+    
+    protected function _ReportError($exc) {
+        $message = array(
+            'errors' => array(
+                'exception' => array(
+                    'message' => $exc->getMessage(),
+                    'code' => $exc->getCode()
+                )
+            ),
+            'message' => 'Internal Server Error',
+            'code' => 500
+        );
+        throw new InternalErrorException(json_encode($message));
+    }
+    
+    protected function _ReportMethodNotAllowed($expected, $action = null) {
+        $message = array(
+            'message' => "Expected HTTP Method: $expected",
+            'code' => 405
+        );
+        if($action) {
+            $message['action'] = $action;
+        }
+        throw new MethodNotAllowedException(json_encode($message));
+    }
+    
+    protected function _ReportUnsupportedMethod() {
+        $message = array(
+            'message' => "Unsupported Method",
+            'code' => 405
+        );
+        throw new MethodNotAllowedException(json_encode($message));
+    }
+
+}
---
+++ app/Controller/UsersController.php
@@ -0,0 +1,147 @@
+<?php
+
+App::import('Controller', 'REST');
+
+class UsersController extends RESTController {
+
+    public function index() {
+        parent::index();
+        
+        $this->User->recursive = 0;
+        $result = $this->find('all');
+        
+        $this->_setResponseJSON(Set::remove($result, '{n}.User.password'));
+    }
+
+    public function view($id = null) {
+        parent::view($id);
+        
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            $this->_ReportNotExistingUser($id);
+        }
+
+        $result = $this->User->read(null, $id);
+        $this->_setResponseJSON(Set::remove($result, 'User.password'));
+    }
+
+    public function add() {
+        parent::add();
+        
+        $this->_CheckUniqueUsername($this->request->data['username']);
+
+        $this->User->create();
+        $this->User->set($this->request->data);
+        if ($this->User->validates()) {
+            
+            try {
+                $user = $this->User->save();
+            } catch (Exception $exc) {
+                $this->_ReportError($exc);
+            }
+            $this->_setResponseJSON(Set::remove($user, 'User.password'));
+            
+        } else {
+            $this->_ReportValidationErrors($this->User->validationErrors);
+        }
+        
+    }
+
+    public function edit($id = null) {
+        parent::edit($id);
+        
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            $this->_ReportNotExistingUser($id);
+        }
+
+        $this->User->set($this->request->input('json_decode'));
+        if ($this->User->validates()) {
+
+            try {
+                $user = $this->User->save();
+            } catch (Exception $exc) {
+                $this->_ReportError($exc);
+            }
+            $this->_setResponseJSON(Set::remove($user, 'User.password'));
+
+        } else {
+            $this->_ReportValidationErrors($this->User->validationErrors);
+        }
+    }
+
+    public function delete($id = null) {
+        parent::delete($id);
+        
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            $this->_ReportNotExistingUser($id);
+        }
+        if ($this->User->delete()) {
+            $this->_setResponseJSON('User deleted');
+        } else {
+            $this->_setResponseJSON('User Not deleted');
+        }
+        
+    }
+
+    public function login() {
+        if ($this->request->is('post')) {
+            $loggedIn = $this->Auth->login();
+            
+            
+            $this->_setResponseJSON(array(
+                'logged'=>$loggedIn, 
+                'user'=>$this->Auth->user()
+                )
+            );
+        } else {
+            $this->_ReportMethodNotAllowed("POST", 'login');
+        }
+    }
+
+    public function logout() {
+        $this->Auth->logout();
+        $this->_setResponseJSON(array(
+                'logged'=>false, 
+                'user'=>null
+            )
+        );
+    }
+    
+    /**
+     * ERROR HANDLER HELPERS:
+     */
+    
+    private function _ReportNotExistingUser($id){
+        $message = array(
+            'message' => 'Invalid user',
+            'id' => $id,
+            'status' => 404
+        );
+        throw new NotFoundException(json_encode($message));
+    }
+    
+    private function _ReportExistingUser($id){
+        $message = array(
+            'message' => 'User already exists',
+            'id' => $id,
+            'status' => 400
+        );
+        throw new BadRequestException(json_encode($message));
+    }
+
+    private function _CheckUniqueUsername($username) {
+        $user = $this->User->findByUsername($username);
+        if ($user) {
+            $message = array(
+                'error' => array(
+                    'username' => "Username aready exists"
+                ),
+                'status' => 400
+            );
+            throw new BadRequestException(json_encode($message));
+        }
+    }
+
+}
