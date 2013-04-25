89988b254b444ace92e555502c73d740f25e057d
stefanosusini <root@ssazuretest.cloudapp.net>
2013-04-25 13:29:05 +0000

    Base version to clean

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'sA)l!Ft6NuYxk!Dqvu#i_m1zPlvSZYo$ks63et@G');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '67289450482237873861069865584481');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
