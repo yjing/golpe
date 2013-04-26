89c27d1e5eaa1fd45003775bbbeb6dd88ffa8fd1
Stefano Susini <s.susini@gmail.com>
2013-04-26 10:09:14 +0100

    Test JOINS

--- app/Controller/ActivityLogsController.php
+++ app/Controller/ActivityLogsController.php
@@ -96,7 +96,7 @@ class ActivityLogsController extends RESTController {
 //        );
         
         $r = $this->ActivityLog->find('all', array(
-            'fields' => array('ActivityLog.title'),
+//            'fields' => array('ActivityLog.title'),
             'conditions' => array( 'ActivityLog.modified >=' => (time() - (7 * 24 * 60 * 60)) )
         ));
         
