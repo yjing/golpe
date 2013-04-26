f02df7501ebc49f0956aa7fe359677890d50862d
Stefano Susini <s.susini@gmail.com>
2013-04-26 16:40:04 +0100

    Test JOINS

--- app/Model/Behavior/HahManyThroughHABTMBehavior.php
+++ app/Model/Behavior/HahManyThroughHABTMBehavior.php
@@ -32,6 +32,7 @@ class HahManyThroughHABTMBehavior extends ModelBehavior {
         foreach ($this->settings[$model->alias] as $m_name => $m) {
             debug($m['model']->alias);
             debug($m['join_table_name']);
+            debug(Inflector::underscore($model->alias));
         }
         
 //        foreach ($results as $key => $value) {
