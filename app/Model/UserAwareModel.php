502202849de6898a90a3db71e9e27690b796db04
Stefano Susini <s.susini@gmail.com>
2013-04-26 10:59:20 +0100

    Test JOINS

--- app/Model/UserAwareModel.php
+++ app/Model/UserAwareModel.php
@@ -22,7 +22,7 @@ abstract class UserAwareModel extends AppModel {
             $queryData['fields'][] = $this->alias . '.*';
         }
         // Add the namespaced fields and models to be used for authorization
-        $queryData['fields'][] = $this->alias . '.visibility_level as AUTHvisibility_level';
+        $queryData['fields'][] = $this->alias . '.visibility_level as ActivityLog.AUTHvisibility_level';
         $queryData['fields'][] = 'AUTHUser.*';
         $queryData['fields'][] = 'AUTHTeam.*';
         // Add the namespaced joins to retrieve the above-mentioned fields and models
