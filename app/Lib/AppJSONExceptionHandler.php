602aebefcd2eef475b8288a1b9b9416b5832af03
Stefano Susini <s.susini@gmail.com>
2013-04-26 11:06:24 +0100

    Test JOINS

--- app/Lib/AppJSONExceptionHandler.php
+++ app/Lib/AppJSONExceptionHandler.php
@@ -13,17 +13,12 @@ class AppJSONExceptionHandler {
         if(Configure::read('debug') == 2) {
             $err['file'] = $error->getFile();
             $err['line'] = $error->getLine();
-            $err['trace'] = $error->getPrevious();
+            $err['trace'] = array();
             
-//            $prev = $error->getPrevious();
-//            while ($prev != null) {
-//                $err['trace'][] = array(
-//                    'message' => $prev->getMessage(),
-//                    'file' => $prev->getFile(),
-//                    'line' => $prev->getLine()
-//                );
-//                $prev = $prev->getPrevious();
-//            }
+            $t = $error->getTrace();
+            foreach ($t as $value) {
+                debug($t);
+            }
         }
         
         echo json_encode($err);
