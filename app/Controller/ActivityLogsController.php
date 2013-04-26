0cf1a13179db76f986c0193919b39c6052d56d92
Stefano Susini <s.susini@gmail.com>
2013-04-26 07:40:50 +0100

    Set up App Configuration

--- app/Controller/ActivityLogsController.php
+++ app/Controller/ActivityLogsController.php
@@ -22,7 +22,7 @@ class ActivityLogsController extends RESTController {
         
         $r = $db->buildStatement(
             array(
-                'fields' => array('ActivityLog.*, User.id, tu.team_id as team_id, ActivityLog.visibility_level as AUTH_VL'),
+                'fields' => array('ActivityLog.*, User.id, tu.team_id as team_id, ActivityLog.visibility_level as User.AUTH_VL'),
                 'table' => $db->fullTableName($this->ActivityLog),
                 'alias' => 'ActivityLog',
                 'group' => array(),
