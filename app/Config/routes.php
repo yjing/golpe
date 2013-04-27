931c7a34078df1288e8d16cfc38411d700627a0d
Stefano Susini <s.susini@gmail.com>
2013-04-27 07:39:14 +0100

    Test JOINS

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -40,7 +40,7 @@
             array('action' => 'update', 'method' => 'POST', 'id' => true)
         ));
 
-        Router::mapResources(array('users', 'activity_logs'));
+        Router::mapResources(array('users', 'activity_logs', 'comments'));
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
