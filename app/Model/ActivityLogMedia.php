8717065cb0ac22747c56e3095f8c31eea0301740
Stefano Susini <s.susini@gmail.com>
2013-04-28 16:26:50 +0100

    Db Schema update

--- app/Model/ActivityLogMedia.php
+++ app/Model/ActivityLogMedia.php
@@ -5,20 +5,8 @@ class ActivityLogMedia extends AppModel {
     public $useTable = "activity_logs_media";
 
     public $name = 'ActivityLogMedia';
-    
     public $belongsTo = array('ActivityLog','Media');
     
-
-    public function beforeSave($options = array()) {
-        parent::beforeSave($options);
-        if(isset($this->data[$this->alias]['created'])) {
-            unset($this->data[$this->alias]['created']);
-        }
-        if(isset($this->data[$this->alias]['modified'])) {
-            unset($this->data[$this->alias]['modified']);
-        }
-        return $this->data;
-    }
 }
 
 ?>
