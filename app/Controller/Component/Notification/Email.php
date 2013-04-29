566c50ce30d50f277ac5b4bf4174ae24ef421804
Stefano Susini <s.susini@gmail.com>
2013-04-29 17:50:19 +0100

    Db Schema update

--- app/Controller/Component/Notification/Email.php
+++ app/Controller/Component/Notification/Email.php
@@ -5,9 +5,12 @@ class Email implements NotificationProvider {
     public function notify($notifications) {
         $user_ids = array();
         foreach ($notifications as $key => $value) {
-            $ids = split(", ", $value['Notification']['to']);
-            debug($ids);
-            $user_ids = array_merge($user_ids, $ids);
+            $ids = $value['Notification']['to'];
+            if(strlen($ids)>0) {
+                $ids = split(", ", $value['Notification']['to']);
+                debug($ids);
+                $user_ids = array_merge($user_ids, $ids);
+            }
         }
         debug(array_unique($user_ids));
     }
