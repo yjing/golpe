f9c671e5a184f96244ed2bfb72166de1fe01018e
Stefano Susini <s.susini@gmail.com>
2013-04-29 10:31:14 +0100

    Db Schema update

--- app/Model/UserAwareModel.php
+++ app/Model/UserAwareModel.php
@@ -46,7 +46,7 @@ abstract class UserAwareModel extends AppModel {
         $queryData['fields'][] = $this->alias . '.visibility_level as AUTHvisibility_level';
         $queryData['fields'][] = 'AUTHUser.id, AUTHUser.username, AUTHUser.role';
         $queryData['fields'][] = 'AUTHTeam.id, AUTHTeam.name, AUTHTeam.project_id';
-        $queryData['fields'][] = 'Supervisor.id';
+        $queryData['fields'][] = 'Supervisor.supervisor_id as id';
         // Add the namespaced joins to retrieve the above-mentioned fields and models
         $queryData['joins'][] = array(
             'table' => "users",
