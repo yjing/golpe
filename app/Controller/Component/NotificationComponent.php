8f0b064f62dabce36c0a75935f69fb4837f85de1
Stefano Susini <s.susini@gmail.com>
2013-04-29 15:44:32 +0100

    Db Schema update

--- app/Controller/Component/NotificationComponent.php
+++ app/Controller/Component/NotificationComponent.php
@@ -1,13 +1,15 @@
 <?php
 App::uses('Model', 'ActivityLog');
 App::uses('Model', 'Comment');
-App::uses('Model', 'Media');
+App::uses('Model', 'User');
 class NotificationComponent extends Component {
     
     private $ActivityLog = null;
+    private $User = null;
     
     public function initialize(Controller $controller) {
         debug($controller->components['Notification']);
+        $this->User = new User();
     }
     
     public function notify($type, $id, $options = array()) {
@@ -15,10 +17,25 @@ class NotificationComponent extends Component {
         switch ($type) {
             case 'ActivityLog':
                 $this->ActivityLog = new ActivityLog();
-                debug($this->ActivityLog->find('first', array(
+                $element = $this->ActivityLog->find('first', array(
                     'conditions' => array('ActivityLog.id' => $id),
                     'recursive' => -1
-                )));
+                ));
+                debug($element);
+                $team_users = $this->User->find('all', array(
+                    'conditions' => array(
+                        'Team.id' => $element['Team']['id']
+                    ),
+                    'recursive' => -1
+                ));
+                debug($team_users);
+//                $team_users = $this->User->find('all', array(
+//                    'conditions' => array(
+//                        'Team.id' => $element['Team']['id']
+//                    ),
+//                    'recursive' => -1
+//                ));
+//                debug($team_users);
 
                 break;
 
