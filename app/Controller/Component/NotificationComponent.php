5f96a6a6093e83626c0a8b13e23a794a16981f4e
Stefano Susini <s.susini@gmail.com>
2013-04-29 14:47:39 +0100

    Db Schema update

--- app/Controller/Component/NotificationComponent.php
+++ app/Controller/Component/NotificationComponent.php
@@ -3,4 +3,19 @@ class NotificationComponent extends Component {
     public function initialize(Controller $controller) {
         debug($controller->components['Notification']);
     }
+    
+    public function notify($type, $element, $options = array()) {
+        $notification = array();
+        switch ($type) {
+            case 'ActivityLog':
+                debug($type);
+                debug($element);
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    
 }
