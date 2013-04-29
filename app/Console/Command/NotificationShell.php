45cfb49b67bd0326ba15a493f79a1082e946602e
Stefano Susini <s.susini@gmail.com>
2013-04-29 17:05:55 +0100

    Db Schema update

--- app/Console/Command/NotificationShell.php
+++ app/Console/Command/NotificationShell.php
@@ -1,21 +1,12 @@
 <?php
 
+App::import('Component', 'Notification');
 class NotificationShell extends AppShell {
-    
-    public $components = array('Notification');
     
     public function notify() {
         
-        // This user is used to inform the UserAwareModel (Media) that the 
-        // call has been issued by the SHELL and has access to all data.
-        App::uses('CakeSession', 'Model/Datasource');
-        CakeSession::write("Auth.User", array(
-            'id' => '-1',
-            'username' => 'msc.shell',
-            'role' => 'SHELL'
-        ));
-        
-        $this->Notification->notify();
+        $Notification = new NotificationComponent();
+        $Notification->notify();
         
     }
 
