b775018f6041802ac9b0fa163cf103d35c29f13d
Stefano Susini <s.susini@gmail.com>
2013-04-29 17:42:58 +0100

    Db Schema update

--- app/Console/Command/NotificationShell.php
+++ app/Console/Command/NotificationShell.php
@@ -1,11 +1,12 @@
 <?php
 
 App::import('Component', 'Notification');
+require_once '../../../lib/Cake/Controller/ComponentCollection.php';
 class NotificationShell extends AppShell {
     
     public function notify() {
         
-        $Notification = new NotificationComponent(null, null);
+        $Notification = new NotificationComponent(new ComponentCollection(), null);
         $Notification->notify();
         
     }
--- app/Controller/Component/Notification/Email.php
+++ app/Controller/Component/Notification/Email.php
@@ -5,7 +5,7 @@ class Email implements NotificationProvider {
     public function notify($notifications) {
         $user_ids = array();
         foreach ($notifications as $key => $value) {
-            $ids = split($value['Notification']['to'], ', ');
+            $ids = split(", ", $value['Notification']['to']);
             array_merge($user_ids, $ids);
         }
         debug($user_ids);
