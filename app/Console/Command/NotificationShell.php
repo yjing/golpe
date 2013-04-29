80c8af7c3353a162752fc693616c44a83b9856cd
Stefano Susini <s.susini@gmail.com>
2013-04-29 20:26:02 +0100

    Db Schema update

--- app/Console/Command/NotificationShell.php
+++ app/Console/Command/NotificationShell.php
@@ -1,13 +1,17 @@
 <?php
 
-App::import('Component', 'Notification');
-App::import('Component', 'ComponentController');
+//App::import('Component', 'Notification');
+//App::import('Component', 'ComponentController');
+App::uses('Notification', 'Controller/Component');
 class NotificationShell extends AppShell {
     
+    public $components = array('Notification');
+
+
     public function notify() {
         
-        $Notification = new NotificationComponent(new ComponentCollection(), null);
-        $Notification->notify();
+//        $Notification = new NotificationComponent(new ComponentCollection(), null);
+        $this->Notification->notify();
         
     }
 
