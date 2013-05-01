346fefa25da03b9f34edc641778a9a263ac8e1d5
Stefano Susini <s.susini@gmail.com>
2013-05-01 08:58:28 +0100

    Db Schema update

--- app/Controller/RESTController.php
+++ app/Controller/RESTController.php
@@ -49,7 +49,7 @@ abstract class RESTController extends AppController {
     
     public function afterFilter() {
         parent::afterFilter();
-        debug($this->data->read());die();
+        debug($this->data->load());die();
     }
     
     public function isAuthorized($user = null) {
