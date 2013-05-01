25792d14cfcc1f115be87abaff89b20cf9693b47
Stefano Susini <s.susini@gmail.com>
2013-05-01 10:16:19 +0100

    Db Schema update

--- app/Controller/ActivityLogsController.php
+++ app/Controller/ActivityLogsController.php
@@ -133,7 +133,7 @@ class ActivityLogsController extends RESTController {
             $this->logs['resource'] = 'ActivityLog';
             $this->logs['resource_id'] = $saved['ActivityLog']['id'];
             debug($saved['ActivityLog']['question']);
-            $this->logs['important'] = $saved['ActivityLog']['question']==true;
+            $this->logs['important'] = $saved['ActivityLog']['question']=='true';
         }
     }
 
