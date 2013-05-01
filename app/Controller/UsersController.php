7b23789baecf27a088558905d7806076205a5305
Stefano Susini <s.susini@gmail.com>
2013-05-01 21:10:26 +0100

    Db Schema update

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -29,7 +29,9 @@ class UsersController extends RESTController {
 
         $result = $this->User->find('first', array(
             'conditions' => array('User.id' => $id),
-            'supervisor'
+            'supervisor' => array(
+                'fields' => array('username', 'id')
+            )
         ));
         $this->_setResponseJSON(Set::remove($result, 'User.password'));
     }
