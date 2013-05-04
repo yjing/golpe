23907fc4f4eb1dcf37c767ae6d259f6ca4539b75
Stefano Susini <s.susini@gmail.com>
2013-05-04 11:26:54 +0100

    Rewriting Associations Management

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,9 +1,9 @@
 <?php
 
 // app/Model/User.php
-class User extends AppModel {
+class User extends AssociativeModel {
     
-    private static $SUPERVISOR_KEY = "supervisor";
+//    private static $SUPERVISOR_KEY = "supervisor";
     private $supervisor_opt;
     public $hasAndBelongsToMany = array(
         'Team',
@@ -48,155 +48,155 @@ class User extends AppModel {
             )
         )
     );
-    
-    public function beforeSave($options = array()) {
-        if (isset($this->data[$this->alias]['password'])) {
-            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-        }
-        return true;
-    }
-    
-    private static $ASSOCIATIONS_KEY = "associations";
-    private $links;
-    private $models = array();
-    
-    public function beforeFind($queryData) {
-        parent::beforeFind($queryData);
-        
-        $queryData['recursive'] = -1;
-        $this->links = $this->getConfigElement($queryData, self::$ASSOCIATIONS_KEY);
-        
-        return $queryData;
-    }
-    
-    public function afterFind($results, $primary = false) {
-        parent::afterFind($results, $primary);
-        
-        if(isset($this->links)) {
-            foreach ($results as $index => $element) {
-                foreach ($this->links as $association_name => $queryData) {
-                    
-                    $this->normalizeKeyValueToAssociative($association_name, $queryData);
-                    
-                    $asso = $this->findAssociation($association_name);
-                    if(isset($asso)) {
-                        $res = call_user_func( array( $this, $asso['function'] ), 
-                            $association_name,
-                            $asso['config'],
-                            $queryData,
-                            $element
-                        );
-                        
-                        $results[$index][$association_name] = $res;
-                    } else {
-                        throw new InternalErrorException("The $association_name association doesn't exists.");
-                    }
-                }
-            }
-        }
-        
-        return $results;
-    }
-    
-    // IMPORTANT: HAS SIDE EFFECT!!! MODIFY THE INPUT DATA!!!
-    private function normalizeKeyValueToAssociative(&$key, &$value) {
-        if(!is_string($key)) {
-            $key = $value;
-            $value = array();
-        }
-//        return array("association_name" => $key, "queryData" => $value);
-    }
-
-
-    private function getHasAndBelongsToMany($association_name, $association_config, $queryData, $element) {
-        
-        if(array_key_exists($association_config['className'], $this->models)) {
-            $associated_model = $this->models[$association_config['className']];
-        } else {
-            $associated_model = $this->loadModel($association_config['className']);
-            $this->models[$association_config['className']] = $associated_model;
-        }
-        
-        $join = array(
-            'table' => $association_config['joinTable'],
-            'alias' => $association_config['with'],
-            'type' => 'LEFT',
-            'conditions' => $association_config['className'] . '.' . $associated_model->primaryKey 
-                . ' = ' . $association_config['with'] . '.' . $association_config['associationForeignKey']
-        );
-        $conditions = array(
-            $association_config['with'] . '.' . $association_config['foreignKey'] . ' = ' . $element[$this->alias]['id']
-        );
-        
-        $options = array(
-            'joins' => array($join),
-            'conditions' => $conditions,
-            'recursive' => 0,
-        );
-        $nested_associations = $this->getConfigElement($queryData, self::$ASSOCIATIONS_KEY);
-        if(isset($nested_associations) && !empty($nested_associations)) {
-            $options[self::$ASSOCIATIONS_KEY] = $nested_associations;
-        }
-        
-        $fields = $this->getConfigElement($queryData, 'fields');
-        if(isset($fields) && !empty($fields)) {
-            $options['fields'] = $fields;
-        }
-        
-        $res = $associated_model->find('all', $options);
-        
-        // DATA FORMAT
-        $alias = $associated_model->alias;
-        $alias_data = Set::extract("{n}.$alias", $res);
-        $res_without_alias = Set::remove($res, "{n}.$alias");
-        $res = Set::merge($alias_data, $res_without_alias);
-        
-        $unArray_if_single_value = $this->getConfigElement($queryData, 'unArray_if_single_value');
-        if (isset($unArray_if_single_value) && $unArray_if_single_value !== false && count($res) == 1) {
-            $res = $res[0];
-        }
-        
-        return $res;
-        
-    }
-    
-    private function loadModel($model_name) {
-        App::import('Model', $model_name);
-        $class = new ReflectionClass($model_name);
-        return $class->newInstanceArgs();
-    } 
-
-    private function findAssociation($association_name) {
-        if(array_key_exists($association_name, $this->hasOne)) {
-            return array(
-                'type' => 'hasOne',
-                'config' => $this->hasOne[$association_name],
-                'function' => 'getHasOne'
-            );
-        }
-        if(array_key_exists($association_name, $this->hasMany)) {
-            return array(
-                'type' => 'hasMany',
-                'config' => $this->hasMany[$association_name],
-                'function' => 'getHasMany'
-            );
-        }
-        if(array_key_exists($association_name, $this->belongsTo)) {
-            return array(
-                'type' => 'belongsTo',
-                'config' => $this->belongsTo[$association_name],
-                'function' => 'getBelongsTo'
-            );
-        }
-        if(array_key_exists($association_name, $this->hasAndBelongsToMany)) {
-            return array(
-                'type' => 'hasAndBelongsToMany',
-                'config' => $this->hasAndBelongsToMany[$association_name],
-                'function' => 'getHasAndBelongsToMany'
-            );
-        }
-        return null;
-    }
+//    
+//    public function beforeSave($options = array()) {
+//        if (isset($this->data[$this->alias]['password'])) {
+//            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+//        }
+//        return true;
+//    }
+//    
+//    private static $ASSOCIATIONS_KEY = "associations";
+//    private $links;
+//    private $models = array();
+//    
+//    public function beforeFind($queryData) {
+//        parent::beforeFind($queryData);
+//        
+//        $queryData['recursive'] = -1;
+//        $this->links = $this->getConfigElement($queryData, self::$ASSOCIATIONS_KEY);
+//        
+//        return $queryData;
+//    }
+//    
+//    public function afterFind($results, $primary = false) {
+//        parent::afterFind($results, $primary);
+//        
+//        if(isset($this->links)) {
+//            foreach ($results as $index => $element) {
+//                foreach ($this->links as $association_name => $queryData) {
+//                    
+//                    $this->normalizeKeyValueToAssociative($association_name, $queryData);
+//                    
+//                    $asso = $this->findAssociation($association_name);
+//                    if(isset($asso)) {
+//                        $res = call_user_func( array( $this, $asso['function'] ), 
+//                            $association_name,
+//                            $asso['config'],
+//                            $queryData,
+//                            $element
+//                        );
+//                        
+//                        $results[$index][$association_name] = $res;
+//                    } else {
+//                        throw new InternalErrorException("The $association_name association doesn't exists.");
+//                    }
+//                }
+//            }
+//        }
+//        
+//        return $results;
+//    }
+//    
+//    // IMPORTANT: HAS SIDE EFFECT!!! MODIFY THE INPUT DATA!!!
+//    private function normalizeKeyValueToAssociative(&$key, &$value) {
+//        if(!is_string($key)) {
+//            $key = $value;
+//            $value = array();
+//        }
+////        return array("association_name" => $key, "queryData" => $value);
+//    }
+//
+//
+//    private function getHasAndBelongsToMany($association_name, $association_config, $queryData, $element) {
+//        
+//        if(array_key_exists($association_config['className'], $this->models)) {
+//            $associated_model = $this->models[$association_config['className']];
+//        } else {
+//            $associated_model = $this->loadModel($association_config['className']);
+//            $this->models[$association_config['className']] = $associated_model;
+//        }
+//        
+//        $join = array(
+//            'table' => $association_config['joinTable'],
+//            'alias' => $association_config['with'],
+//            'type' => 'LEFT',
+//            'conditions' => $association_config['className'] . '.' . $associated_model->primaryKey 
+//                . ' = ' . $association_config['with'] . '.' . $association_config['associationForeignKey']
+//        );
+//        $conditions = array(
+//            $association_config['with'] . '.' . $association_config['foreignKey'] . ' = ' . $element[$this->alias]['id']
+//        );
+//        
+//        $options = array(
+//            'joins' => array($join),
+//            'conditions' => $conditions,
+//            'recursive' => 0,
+//        );
+//        $nested_associations = $this->getConfigElement($queryData, self::$ASSOCIATIONS_KEY);
+//        if(isset($nested_associations) && !empty($nested_associations)) {
+//            $options[self::$ASSOCIATIONS_KEY] = $nested_associations;
+//        }
+//        
+//        $fields = $this->getConfigElement($queryData, 'fields');
+//        if(isset($fields) && !empty($fields)) {
+//            $options['fields'] = $fields;
+//        }
+//        
+//        $res = $associated_model->find('all', $options);
+//        
+//        // DATA FORMAT
+//        $alias = $associated_model->alias;
+//        $alias_data = Set::extract("{n}.$alias", $res);
+//        $res_without_alias = Set::remove($res, "{n}.$alias");
+//        $res = Set::merge($alias_data, $res_without_alias);
+//        
+//        $unArray_if_single_value = $this->getConfigElement($queryData, 'unArray_if_single_value');
+//        if (isset($unArray_if_single_value) && $unArray_if_single_value !== false && count($res) == 1) {
+//            $res = $res[0];
+//        }
+//        
+//        return $res;
+//        
+//    }
+//    
+//    private function loadModel($model_name) {
+//        App::import('Model', $model_name);
+//        $class = new ReflectionClass($model_name);
+//        return $class->newInstanceArgs();
+//    } 
+//
+//    private function findAssociation($association_name) {
+//        if(array_key_exists($association_name, $this->hasOne)) {
+//            return array(
+//                'type' => 'hasOne',
+//                'config' => $this->hasOne[$association_name],
+//                'function' => 'getHasOne'
+//            );
+//        }
+//        if(array_key_exists($association_name, $this->hasMany)) {
+//            return array(
+//                'type' => 'hasMany',
+//                'config' => $this->hasMany[$association_name],
+//                'function' => 'getHasMany'
+//            );
+//        }
+//        if(array_key_exists($association_name, $this->belongsTo)) {
+//            return array(
+//                'type' => 'belongsTo',
+//                'config' => $this->belongsTo[$association_name],
+//                'function' => 'getBelongsTo'
+//            );
+//        }
+//        if(array_key_exists($association_name, $this->hasAndBelongsToMany)) {
+//            return array(
+//                'type' => 'hasAndBelongsToMany',
+//                'config' => $this->hasAndBelongsToMany[$association_name],
+//                'function' => 'getHasAndBelongsToMany'
+//            );
+//        }
+//        return null;
+//    }
 
 
 //    public function beforeFind($queryData) {
@@ -243,17 +243,17 @@ class User extends AppModel {
 //        
 //        return $results;
 //    }
-    
-    public function getConfigElement($config_array, $key) {
-        $result = null;
-        
-        if(in_array($key, $config_array, true)) {
-            $result = array();
-        } elseif (array_key_exists($key, $config_array)) {
-            $result = $config_array[$key];
-        }
-        return $result;
-    }
+//    
+//    public function getConfigElement($config_array, $key) {
+//        $result = null;
+//        
+//        if(in_array($key, $config_array, true)) {
+//            $result = array();
+//        } elseif (array_key_exists($key, $config_array)) {
+//            $result = $config_array[$key];
+//        }
+//        return $result;
+//    }
     
     public function getTeam($userId){
         $query = "select Team.* 
