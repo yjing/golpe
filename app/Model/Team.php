0516c002d07d442daba643c7c063d96b5efaee1c
Stefano Susini <s.susini@gmail.com>
2013-05-04 19:12:02 +0100

    Rewriting Associations Management

--- app/Model/Team.php
+++ app/Model/Team.php
@@ -3,7 +3,14 @@
 App::import('Model', 'AssociativeModel');
 class Team extends AssociativeModel {
     public $belongsTo = array('Project');
-    public $hasAndBelongsToMany = array('User');
+    public $hasAndBelongsToMany = array(
+        'Students' => array(
+            'className' => 'User',
+            'joinTable' => 'teams_users',
+            'foreignKey' => 'user_id',
+            'associationForeignKey' => 'team_id'
+        )
+    );
 }
 
 ?>
