4d7d02ca2c0c33df6a80dcfc294f54ad7ab3a545
Stefano Susini <s.susini@gmail.com>
2013-05-04 12:21:50 +0100

    Rewriting Associations Management

--- app/Model/ActivityLog.php
+++ app/Model/ActivityLog.php
@@ -8,16 +8,17 @@ App::import('Behavior', 'HasMedia');
 
 // app/Model/User.php
 //class ActivityLog extends AppModel {
-class ActivityLog extends UserAwareModel {
+//class ActivityLog extends UserAwareModel {
+class ActivityLog extends AssociativeModel {
 
     public $name = 'ActivityLog';
     public $useTable = "activity_logs";
-    public $actsAs = array('Containable', 'HasMedia', 
-        'HahManyThroughHABTM' => array(
-            'Media' => array(), 
-            'Comment' => array()
-        )
-    );
+//    public $actsAs = array('Containable', 'HasMedia', 
+//        'HahManyThroughHABTM' => array(
+//            'Media' => array(), 
+//            'Comment' => array()
+//        )
+//    );
     
 }
 
