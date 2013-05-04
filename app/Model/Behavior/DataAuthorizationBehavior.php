dcedf9d25cdf4cb6e8b55515b684af73079a8e76
Stefano Susini <s.susini@gmail.com>
2013-05-04 18:39:21 +0100

    Rewriting Associations Management

--- app/Model/Behavior/DataAuthorizationBehavior.php
+++ app/Model/Behavior/DataAuthorizationBehavior.php
@@ -5,14 +5,16 @@
  * @author susini
  */
 class DataAuthorizationBehavior extends ModelBehavior {
+    
+    private $config;
+    
     public function beforeFind(Model $model, $query) {
         parent::beforeFind($model, $query);
-        
-        debug("TestCBBehavior beforeFind");
+        $this->config = array($model->alias . ' ' . rand(5, 15));
     }
     public function afterFind(Model $model, $results, $primary) {
         parent::afterFind($model, $results, $primary);
         
-        debug("TestCBBehavior afterFind");
+        debug($this->config);
     }
 }
