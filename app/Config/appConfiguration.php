f73222f1546c32baf0698761de8951c8a37820cd
Stefano Susini <s.susini@gmail.com>
2013-05-05 13:31:31 +0100

    Rewriting Data Access Management

--- app/Config/appConfiguration.php
+++ app/Config/appConfiguration.php
@@ -66,7 +66,7 @@ Configure::write("APPCONFIG", array(
                 'StudentsSupervisor.supervisor_id' => '@(User.Supervisor.id)',
                 'OR' => array(
                    'AND' => array(
-                       '@(User.Team.id)  IS NULL dddddd',
+                       '@(User.Team.id)  IS NULL',
                         '#MainResource#.visibility_level NOT IN' => array('PRIVATE', 'SUPERVISOR')
                     ),
                     'AND' => array(
