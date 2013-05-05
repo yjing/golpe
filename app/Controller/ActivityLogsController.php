0389d069bf169eee3fd1bb8468c74e843aa5996b
Stefano Susini <s.susini@gmail.com>
2013-05-05 18:31:02 +0100

    Rewriting Data Access Management

--- app/Controller/ActivityLogsController.php
+++ app/Controller/ActivityLogsController.php
@@ -127,7 +127,9 @@ class ActivityLogsController extends RESTController {
                         'associations' => array(
                             'Supervisor' => array()
                         )
-                    )
+                    ),
+                    'Media',
+                    'Comment'
                 )
             )
         ));
