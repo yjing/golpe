8ae1b325344a582acf2a337c6d7548e6b56b4068
Stefano Susini <s.susini@gmail.com>
2013-05-05 17:27:01 +0100

    Rewriting Data Access Management

--- app/Controller/Component/NotificationComponent.php
+++ app/Controller/Component/NotificationComponent.php
@@ -82,6 +82,7 @@ class NotificationComponent extends Component {
         $this->User = new User();
         switch ($type) {
             case 'ActivityLog':
+                Configure::write('debug', 0);
                 $this->ActivityLog = new ActivityLog();
                 $element = $this->ActivityLog->find('first', array(
                     'conditions' => array('ActivityLog.id' => $id),
