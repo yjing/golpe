e9761a86968c467b2e0d1c0eb97aea756001fa20
Stefano Susini <s.susini@gmail.com>
2013-05-05 16:56:10 +0100

    Rewriting Data Access Management

--- app/Model/Media.php
+++ app/Model/Media.php
@@ -4,7 +4,8 @@ App::import("Model", "AssociativeModel");
 class Media extends AssociativeModel {
 
     public $name = 'Media';
-    
     public $belongsTo = "User";
+    
+    public $actsAs = array('DataAuthorization');
 }
 ?>
