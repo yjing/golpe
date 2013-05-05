cf894595ce52ec725d57f204bc8eec7a6b3af660
Stefano Susini <s.susini@gmail.com>
2013-05-05 15:45:27 +0100

    Rewriting Data Access Management

--- app/Model/Behavior/DataAuthorizationBehavior.php
+++ app/Model/Behavior/DataAuthorizationBehavior.php
@@ -48,7 +48,7 @@ class DataAuthorizationBehavior extends ModelBehavior {
         $joins_config = $this->getConfigElement($this->config, 'joins');
         $model_joins = $this->getConfigElement($this->config, $model->alias);
         
-        debug($model->alias);
+        debug($this->config);
         debug($joins_config);
         debug($model_joins);
         
