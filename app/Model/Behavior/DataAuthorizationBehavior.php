5bb92446eba4daf9e36a5684b59736875d57475d
Stefano Susini <s.susini@gmail.com>
2013-05-05 18:38:30 +0100

    Rewriting Data Access Management

--- app/Model/Behavior/DataAuthorizationBehavior.php
+++ app/Model/Behavior/DataAuthorizationBehavior.php
@@ -56,7 +56,6 @@ class DataAuthorizationBehavior extends ModelBehavior {
         
         $this->main_resource_name = $model->alias;
         
-        debug($this->logged_user['User']['role']);
         if(in_array($this->logged_user['User']['role'], Configure::read("APPCONFIG.super_roles"))) {
             debug("SUPERUSER");
             return $query;
