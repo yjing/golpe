13649f655ea9ae0d4ed0aad23ec6f999c3ccfb94
Stefano Susini <s.susini@gmail.com>
2013-05-05 07:41:07 +0100

    Rewriting Data Access Management

--- app/Model/Behavior/DataAuthorizationBehavior.php
+++ app/Model/Behavior/DataAuthorizationBehavior.php
@@ -149,7 +149,7 @@ class DataAuthorizationBehavior extends ModelBehavior {
             );
         }
         if(array_key_exists($association_name, $model->hasAndBelongsToMany)) {
-            debug("HABTM");
+            debug($model->hasAndBelongsToMany[$association_name]);
             return array(
                 'type' => 'hasAndBelongsToMany',
                 'config' => $model->hasAndBelongsToMany[$association_name],
