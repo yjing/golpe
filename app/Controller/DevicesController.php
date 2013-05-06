166b86c8036ef3027896cd2b92454149f9a6db75
Stefano Susini <s.susini@gmail.com>
2013-05-06 16:54:17 +0100

    Debugging File Uploads

--- app/Controller/DevicesController.php
+++ app/Controller/DevicesController.php
@@ -23,6 +23,8 @@ class DevicesController extends RESTController {
             )
         ));
         
+        debug($result);
+        $result = "{\"hello\"}";
         $this->_setResponseJSON($result);
     }
 
