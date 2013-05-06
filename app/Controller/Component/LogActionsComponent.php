31ca3303648bbe7cd046bb6f03d18d451a07e5ee
Stefano Susini <s.susini@gmail.com>
2013-05-06 09:41:01 +0100

    Debugging File Uploads

--- app/Controller/Component/LogActionsComponent.php
+++ app/Controller/Component/LogActionsComponent.php
@@ -1,13 +1,19 @@
 <?php
 
 class LogActionsComponent extends Component {
+    
+    private $config;
+    
     public function startup(Controller $controller) {
         parent::startup($controller);
-        debug($controller->action);
+        if(isset($controller->components[$this->name])) {
+            $this->config = $controller->components[$this->name];
+        }
     }
     
     public function beforeRender(Controller $controller) {
         parent::beforeRender($controller);
+        debug($this->config);
         debug($controller->action);
     }
 }
