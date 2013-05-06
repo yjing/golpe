7fc5916fc2e4e6add339f41b8f123e7e5c65f6e0
Stefano Susini <s.susini@gmail.com>
2013-05-06 12:55:29 +0100

    Debugging File Uploads

--- app/Controller/Component/NotificationComponent.php
+++ app/Controller/Component/NotificationComponent.php
@@ -100,7 +100,9 @@ class NotificationComponent extends Component {
                     )
                 )
             ));
-            debug($element);
+            debug(Set::extract("/$model->alias/visibility_level", $element));
+            debug(Set::extract("/$model->alias/title", $element));
+            debug(Set::extract("/$model->alias/User/Supervisor/supervisor_id", $element));
             die();
         }
         switch ($type) {
