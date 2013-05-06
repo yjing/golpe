a64e6b0b747f90643fbd33aec07833d36e332fbb
Stefano Susini <s.susini@gmail.com>
2013-05-06 20:33:11 +0100

    Debugging File Uploads

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -4,6 +4,5 @@ App::import('Model', 'AssociativeModel');
 class Project extends AssociativeModel {
     
     public $hasMany = array('Team');
-    public $actsAs = array('DataAuthorization');
     
 }
