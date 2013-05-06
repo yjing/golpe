81e1c60fbe4574c6f60891920ca5a55e75b8975b
Stefano Susini <s.susini@gmail.com>
2013-05-06 13:42:26 +0100

    Debugging File Uploads

--- app/Controller/Component/NotificationComponent.php
+++ app/Controller/Component/NotificationComponent.php
@@ -112,84 +112,23 @@ class NotificationComponent extends Component {
                         $notification['Notification']['resource'] = "ActivityLog:$res_id";
                         $notification['Notification']['message'] = Set::get($element, "/ActivityLog/title");
                         $notification['Notification']['priority'] = Set::get($element, "/ActivityLog/question") == true;
+                        
                         break;
 
-                    default:
+                    case 'Comment':
+                        //message & priority
+                        $res_id = Set::get($element, "/Comment/id");
+                        $notification['Notification']['resource'] = "Comment:$res_id";
+                        $notification['Notification']['message'] = Set::get($element, "/Comment/content");
+                        $notification['Notification']['priority'] = false;
+//                        $notification['Notification']['priority'] = Set::get($element, "/ActivityLog/question") == true;
+                        
                         break;
                 }
                 debug($notification);die();
             }
-            debug(Set::get($element, "/$model->alias/visibility_level"));
-            debug(Set::get($element, "/$model->alias/title"));
-            debug(Set::get($element, "/$model->alias/User/Supervisor/id"));
-            debug(Set::extract($element, "/$model->alias/User/Team/Student/id"));
-            die();
         }
-        switch ($type) {
-            case 'ActivityLog':
-                $this->ActivityLog = new ActivityLog();
-                $element = $this->ActivityLog->find('first',
-                    array(
-                        'conditions' => array(
-                            'ActivityLog.id' => $id
-                        ),
-                        'recursive' => -1,
-                        'associations' => array(
-                            'User' => array(
-                                'associations' => array(
-                                    'Supervisor' => array("unArray_if_single_value"),
-                                    'Team' => array(
-                                        "unArray_if_single_value",
-                                        'associations' => array(
-                                            'Student'
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                );
-                
-//                debug($element);
-//                die();
-                
-                $visibility_level = $element['ActivityLog']['visibility_level'];
-                if($visibility_level == 'PRIVATE') {
-                    break;
-                }
-                
-                $notification['Notification']['message'] = $element['ActivityLog']['title'];
-                if($element['ActivityLog']['question']) {
-                    $notification['Notification']['priority'] = true;
-                }
-                
-                if(in_array($visibility_level, array('SUPERVISOR'))) {
-                    $notification_users[] = $element['ActivityLog']['User']['Supervisor']['id'];
-                }
-                
-                if(in_array($visibility_level, array('TEAM'))) {
-                    // TEAM AL go also to Supervisor
-                    $notification_users[] = $element['ActivityLog']['User']['Supervisor']['id'];
-                    
-//                    $team_users = $this->User->getTeamComponents($element['ActivityLog']['User']['Team']['id']);
-                    foreach ($element['ActivityLog']['User']['Team']['Student'] as $key => $value) {
-                        $notification_users[] = $value['id'];
-                    }
-                }
-                
-                if($visibility_level == 'PUBLIC') {
-                    $notification['Notification']['public'] = true;
-                }
-                $notification['Notification']['to'] = implode($notification_users, ', ');
-                
-                $this->Notification = new Notification();
-                $result = $this->Notification->save($notification);
-
-                break;
-
-            default:
-                break;
-        }
+        
     }
     
     private function generateRecipients($element, $model){
