453cb595085a6e45f17baea64783f7342f361e7f
Stefano Susini <s.susini@gmail.com>
2013-05-06 11:22:44 +0100

    Debugging File Uploads

--- app/Model/Media.php
+++ app/Model/Media.php
@@ -5,7 +5,7 @@ class Media extends AssociativeModel {
 
     public $name = 'Media';
     public $belongsTo = "User";
-    
+    public $hasAndBelongsToMany = array('Comment', 'ActivityLog');
     public $actsAs = array('DataAuthorization');
 }
 ?>
