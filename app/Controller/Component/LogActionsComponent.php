f6bcb9d5defba10a670e24e0be385cbede2ba11a
Stefano Susini <s.susini@gmail.com>
2013-05-06 09:47:19 +0100

    Debugging File Uploads

--- app/Controller/Component/LogActionsComponent.php
+++ app/Controller/Component/LogActionsComponent.php
@@ -6,14 +6,29 @@ class LogActionsComponent extends Component {
     
     public function startup(Controller $controller) {
         parent::startup($controller);
-        if(isset($controller->components['LogActions'])) {
-            $this->config = $controller->components['LogActions'];
-        }
+        $this->config = $this->getConfigElement($controller->components, 'LogActions', true);
     }
     
     public function beforeRender(Controller $controller) {
         parent::beforeRender($controller);
-        debug($this->config);
+        debug($controller->{'action'});
         debug($controller->action);
+    }
+    
+    
+    public function getConfigElement($config_array, $key, $always_return_array) {
+        $result = null;
+        if($always_return_array) {
+            $result = array();
+        }
+        
+        if(isset($config_array)) {
+            if(in_array($key, $config_array, true)) {
+                $result = array();
+            } elseif (array_key_exists($key, $config_array)) {
+                $result = $config_array[$key];
+            }
+        }
+        return $result;
     }
 }
