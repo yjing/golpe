86afa28702cdb855d8102fd1ed4dfc3de4c885cb
Stefano Susini <s.susini@gmail.com>
2013-05-06 16:54:40 +0100

    Debugging File Uploads

--- app/Controller/DevicesController.php
+++ app/Controller/DevicesController.php
@@ -14,16 +14,16 @@ class DevicesController extends RESTController {
     public function index() {
         parent::index();
         
-        $result = $this->Device->find('all', array(
-            'fields' => array('id', 'user_id'),
-            'associations' => array(
-                'DeviceProperty' => array(
-                    'fields' => array('key', 'value')
-                )
-            )
-        ));
-        
-        debug($result);
+//        $result = $this->Device->find('all', array(
+//            'fields' => array('id', 'user_id'),
+//            'associations' => array(
+//                'DeviceProperty' => array(
+//                    'fields' => array('key', 'value')
+//                )
+//            )
+//        ));
+//        
+//        debug($result);
         $result = "{\"hello\"}";
         $this->_setResponseJSON($result);
     }
