32fa487d1079e70d5b038e841a4a60274ebd21b2
Stefano Susini <s.susini@gmail.com>
2013-05-06 17:18:45 +0100

    Debugging File Uploads

--- app/Model/Device.php
+++ app/Model/Device.php
@@ -5,7 +5,7 @@ class Device extends AssociativeModel {
 
     public $name = 'Device';
     public $useTable = "devices";
-    public $belongsTo = array('User');
+//    public $belongsTo = array('User');
     public $hasMany = array('DeviceProperty');
     public $actsAs = array('DataAuthorization');
 }
