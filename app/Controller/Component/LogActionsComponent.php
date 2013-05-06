bc091dfc2c32f6d8800048d2928bd72c3d6b5615
Stefano Susini <s.susini@gmail.com>
2013-05-06 10:02:54 +0100

    Debugging File Uploads

--- app/Controller/Component/LogActionsComponent.php
+++ app/Controller/Component/LogActionsComponent.php
@@ -3,20 +3,51 @@
 class LogActionsComponent extends Component {
     
     private $config;
+    private $log_id;
+    
+    private $resource_name;
+    private $resource_id = null;
+    private $important = false;
+    private $action_rerult = false;
     
     public function startup(Controller $controller) {
         parent::startup($controller);
+        
         $this->config = $this->getConfigElement($controller->components, 'LogActions', true);
-        debug($controller->action);
+        $this->log_actions = $this->getConfigElement($this->config, 'log_actions', true);
+        if(in_array($controller->action, $this->log_actions)) {
+            $this->resource_name = $controller->modelClass;
+            $this->resource_id = null;
+            $this->important = false;
+            $this->action_rerult = false;
+        }
     }
     
     public function beforeRender(Controller $controller) {
         parent::beforeRender($controller);
-        debug($controller->response->statusCode());
-        debug($controller->action);
+        if(in_array($controller->action, $this->log_actions)) {
+            debug($controller->response->statusCode());
+            debug($controller->action);
+        }
     }
     
+    public function setResourceName($res_name) {
+        $this->resource_name = $res_name;
+    }
     
+    public function setResourceId($res_id) {
+        $this->resource_id = $res_id;
+    }
+    
+    public function setImportant($important) {
+        $this->important = $important;
+    }
+    
+    public function setActionResult($result) {
+        $this->action_rerult = $result;
+    }
+
+
     public function getConfigElement($config_array, $key, $always_return_array) {
         $result = null;
         if($always_return_array) {
