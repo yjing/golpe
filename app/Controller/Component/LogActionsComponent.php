51502352afc2a2cc0fde48524ccb25abfbc9f671
Stefano Susini <s.susini@gmail.com>
2013-05-06 10:37:58 +0100

    Debugging File Uploads

--- app/Controller/Component/LogActionsComponent.php
+++ app/Controller/Component/LogActionsComponent.php
@@ -58,8 +58,6 @@ class LogActionsComponent extends Component {
     public function beforeRender(Controller $controller) {
         parent::beforeRender($controller);
         if(count($this->log_actions) == 0 || in_array($controller->action, $this->log_actions)) {
-            
-            debug($controller->response->statusCode());
             $log = array(
                 'Log' => array(
                     'id' => $this->log_id,
