7cd51207a94a425c341bd53d49634f7ef781d3e2
Stefano Susini <s.susini@gmail.com>
2013-05-06 09:02:53 +0100

    Debugging File Uploads

--- app/Console/Command/UploaderShell.php
+++ app/Console/Command/UploaderShell.php
@@ -35,35 +35,39 @@ class UploaderShell extends AppShell {
         ));
         
         $meds = Set::extract('/Media/.', $meds);
-        
+        $uploads_dir = '/ulpoads';
         foreach ($meds as $key => $m) {
-            $media_id = $m['id'];
             
-            $media = array(
-                'id' => $media_id,
-                'filename' => $m['filename'],
-                'type' => $m['content-type'],
-                'size' => $m['content-size'],
-                'file' => "/uploads/$media_id",
-                'lockfile' => "/uploads/$media_id.lock",
-                'hasthumb' => $m['has_thumb']
-            );
-            if($m['has_thumb']) {
-                $media['bthumb'] = "/uploads/$media_id.200.thumb";
-                $media['bthumb_fname'] = "$media_id.200.thumb";
-                $media['sthumb'] = "/uploads/$media_id.75.thumb";
-                $media['sthumb_fname'] = "$media_id.75.thumb";
+            $media_id = $m['id'];
+            $filename = $m['filename'];
+            $type = $m['content-type'];
+            $size = $m['content-size'];
+            $lockfile_path = "/uploads/$media_id.lock";
+            $has_thumb = $m['has_thumb'];
+            $b_thumb = null;
+            $s_thumb = null;
+            if($has_thumb) {
+                $b_thumb = "$media_id.200.thumb";
+                $s_thumb = "$media_id.75.thumb";
             }
             
-            debug($media);
-            
-            $lockfile_handle = fopen($media['lockfile'], 'c');
+            $lockfile_handle = fopen($lockfile_path, 'c');
             if( flock( $lockfile_handle, LOCK_EX+LOCK_NB ) ) {
                 
                 // CHECK FILES
-                if(file_exists($media['file'])) {
-                    if($media['hasthumb'] && (!file_exists($media['bthumb']) || !file_exists($media['sthumb']))){
+                if(file_exists("$uploads_dir/$media_id")) {
+                    $b_thumb_file_exists = file_exists("$uploads_dir/$b_thumb");
+                    $s_thumb_file_exists = file_exists("$uploads_dir/$s_thumb");
+                    if($has_thumb && (!$b_thumb_file_exists || !$s_thumb_file_exists)){
                         // MINOR ERROR -> delete thumbs and change DB
+                        $has_thumb = false;
+                        if($b_thumb_file_exists) {
+                            unlink("$uploads_dir/$b_thumb");
+                        }
+                        if($s_thumb_file_exists) {
+                            unlink("$uploads_dir/$s_thumb");
+                        }
+                        echo "Warning: media thumbs don't exists...";
                     }
                     
                     // Create BLOB SERVICE.
@@ -75,104 +79,111 @@ class UploaderShell extends AppShell {
                     $uploaded = $this->uploafFile(
                         $blobRestProxy, 
                         $uploading_file_handler, 
-                        $media['type'], 
-                        $media['id'], 
+                        $type, 
+                        $media_id, 
                         3
                     );
                     if($uploaded) {
                         echo "Upload: $media_id done...";
                         
-                        $b_thumb_handler = fopen($media['bthumb'], 'r');
-                        $up_b_thumb = $this->uploafFile(
-                            $blobRestProxy, 
-                            $b_thumb_handler, 
-                            "image/png", 
-                            $media['bthumb_fname'], 
-                            3
-                        );
-                        $filename = $media['bthumb'];
-                        if(!$up_b_thumb) {
-                            // WARNING
-                            echo "Warning: $filename can't be uploaded...\n";
-                            echo "Warning: the thumb will be unavailable\n";
-                        } else {
-                            echo "Upload: $filename done...";
-                        }
-                        
-                        $s_thumb_handler = fopen($media['sthumb'], 'r');
-                        $up_s_thumb = $this->uploafFile(
-                            $blobRestProxy, 
-                            $s_thumb_handler, 
-                            "image/png", 
-                            $media['sthumb_fname'], 
-                            3
-                        );
-                        $filename = $media['sthumb'];
-                        if(!$up_s_thumb) {
-                            // WARNING
-                            echo "Warning: $filename can't be uploaded...\n";
-                            echo "Warning: the thumb will be unavailable\n";
-                        } else {
-                            echo "Upload: $filename done...";
+                        $up_b_thumb = false;
+                        $up_s_thumb = false;
+                        if($has_thumb) {
+                            
+                            $b_thumb_handler = fopen("$uploads_dir/$b_thumb", 'r');
+                            $up_b_thumb = $this->uploafFile(
+                                $blobRestProxy, 
+                                $b_thumb_handler, 
+                                "image/png", 
+                                $b_thumb, 
+                                3
+                            );
+                            
+                            if($up_b_thumb) {
+                                echo "Upload: $b_thumb done...";
+                                
+                                $s_thumb_handler = fopen("$uploads_dir/$s_thumb", 'r');
+                                $up_s_thumb = $this->uploafFile(
+                                    $blobRestProxy, 
+                                    $s_thumb_handler, 
+                                    "image/png", 
+                                    $s_thumb, 
+                                    3
+                                );
+                                
+                                if($up_s_thumb) {
+                                    echo "Upload: $s_thumb done...";
+                                } else {
+                                    // WARNING
+                                    $has_thumb = false;
+                                    // REMOVE THE UPLOADED BLOB
+                                    $blobRestProxy->deleteBlob("usermedia", $b_thumb);
+                                    echo "Warning: thumbs can't be uploaded...\n";
+                                    echo "Warning: the thumb will be unavailable\n";
+                                }
+                            } else {
+                                // WARNING
+                                $has_thumb = false;
+                                echo "Warning: thumbs can't be uploaded...\n";
+                                echo "Warning: the thumb will be unavailable\n";
+                            }
                         }
                         
                         // UPDATE MEDIA
                         $updated_media = array(
-                            'id' => $media['id'],
+                            'id' => $media_id,
                             'status' => self::$mediaStatus_AVAILABLE,
-                            'has_thumb' => ($up_b_thumb && $up_s_thumb)
+                            'has_thumb' => $has_thumb
                         );
                         
                         $saved = $this->Media->save($updated_media);
+                        debug($saved);
+                        echo '\n';
+                        
                         
                         if($saved) {
                             echo "Media $media_id updated...";
                             
                             // FILES ARE NO MORE NECESSARY, MEDIA IS AVAILABLE
                             if(flock($uploading_file_handler, LOCK_EX)){
-                                unlink($media['file']);
+                                unlink("$uploads_dir/$media_id");
                                 flock($uploading_file_handler, LOCK_UN);
                                 fclose($uploading_file_handler);
                             }
-                            if(flock($b_thumb_handler, LOCK_EX)){
-                                unlink($media['bthumb']);
-                                flock($b_thumb_handler, LOCK_UN);
-                                fclose($b_thumb_handler);
-                            }
-                            if(flock($s_thumb_handler, LOCK_EX)){
-                                unlink($media['sthumb']);
-                                flock($s_thumb_handler, LOCK_UN);
-                                fclose($s_thumb_handler);
-                            }
-                            
-                            if(!$media['has_thumb']) {
-                                if(!$up_b_thumb) {
-                                    $filename = $media['bthumb_fname'];
-                                    $blobRestProxy->deleteBlob("usermedia", $filename);
-                                    echo "$filename removed from container...";
+                            if($has_thumb) {
+                                if(isset($b_thumb_handler) && 
+                                        flock($b_thumb_handler, LOCK_EX)){
+                                    unlink("$uploads_dir/$b_thumb");
+                                    flock($b_thumb_handler, LOCK_UN);
+                                    fclose($b_thumb_handler);
                                 }
-                                if(!$up_s_thumb) {
-                                    $filename = $media['sthumb_fname'];
-                                    $blobRestProxy->deleteBlob("usermedia", $filename);
-                                    echo "$filename removed from container...";
+                                if(isset($s_thumb_handler) && 
+                                        flock($s_thumb_handler, LOCK_EX)){
+                                    unlink("$uploads_dir/$s_thumb");
+                                    flock($s_thumb_handler, LOCK_UN);
+                                    fclose($s_thumb_handler);
                                 }
                             }
                             
                         } else {
                             // ERROR
                             fclose($uploading_file_handler);
-                            fclose($b_thumb_handler);
-                            fclose($s_thumb_handler);
+                            if(isset($b_thumb_handler)){
+                                fclose($b_thumb_handler);
+                            }
+                            if(isset($s_thumb_handler)){
+                                fclose($s_thumb_handler);
+                            }
                             
                             $blobRestProxy->deleteBlob("usermedia", $media_id);
                             if($up_b_thumb) {
-                                $blobRestProxy->deleteBlob("usermedia", $media['bthumb_fname']);
+                                $blobRestProxy->deleteBlob("usermedia", $b_thumb);
                             }
                             if($up_s_thumb) {
-                                $blobRestProxy->deleteBlob("usermedia", $media['sthumb_fname']);
+                                $blobRestProxy->deleteBlob("usermedia", $s_thumb);
                             }
                             
-                            echo "Error: $media_id can't be UPDATED...\n";
+                            echo "Error: $media_id media can't be UPDATED...\n";
                         }
                         
                         
@@ -180,17 +191,15 @@ class UploaderShell extends AppShell {
                         // ERROR
                         fclose($uploading_file_handler);
                         
-                        $filename = $media['file'];
-                        echo "Error: $filename can't be uploaded...\n";
+                        echo "Error: $uploads_dir/$media_id can't be uploaded...\n";
                     }
                     
                 } else {
                     // ERROR
-                    $filename = $media['file'];
-                    echo "Error: $filename doesn't exist...\n";
+                    echo "Error: $uploads_dir/$media_id doesn't exist...\n";
                 }
                 
-                unlink($media['lockfile']);
+                unlink($lockfile_path);
                 flock($lockfile_handle, LOCK_UN);
             }
             fclose($lockfile_handle);
