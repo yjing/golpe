8ca838b9b8d052ca5021ac21833d5f38b423ea1e
Stefano Susini <s.susini@gmail.com>
2013-05-06 23:38:23 +0100

    Projects and Teams management

--- app/Model/TeamUser.php
+++ app/Model/TeamUser.php
@@ -5,7 +5,6 @@ class TeamUser extends AssociativeModel {
     
     public $name = 'TeamUser';
     public $useTable = "teams_users";
-    public $primaryKey = array('user_id', 'team_id');
     public $belongsTo = array('Team', 'User');
 }
 
