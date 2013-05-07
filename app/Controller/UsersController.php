452d32d05bc52ac02deb5760c4c81f810f902056
Stefano Susini <s.susini@gmail.com>
2013-05-07 11:06:21 +0100

    Projects and Teams management

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -64,6 +64,7 @@ class UsersController extends RESTController {
     public function update($id = null) {
         parent::update($id);
         debug($this->request->data);
+        debug($this->request->input());
         if($this->request->data) {
             
             $this->User->id = $id;
