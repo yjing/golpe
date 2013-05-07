eddda4d3e6e6324aedadf4d8aa9f12518cbbccda
Stefano Susini <s.susini@gmail.com>
2013-05-07 20:21:41 +0100

    Projects and Teams management

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -5,4 +5,13 @@ class Project extends AssociativeModel {
     
     public $hasMany = array('Team');
     
+    public $validate = array(
+        'name' => array(
+            'requiredName' => array (
+                'required' => true,
+                'message' => 'Project name must be at least present.'
+            )
+        )
+    );
+    
 }
