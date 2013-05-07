1662ebe4de7c7c1c99fccf48a56d905a7afb5c52
Stefano Susini <s.susini@gmail.com>
2013-05-07 20:40:24 +0100

    Projects and Teams management

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -7,11 +7,18 @@ class Project extends AssociativeModel {
     
     public $validate = array(
         'name' => array(
-            'rule' => 'notEmpty',
-            'required' => true,
-            'allowEmpty' => false,
-            'message' => 'Project name must be present.'
-        )
+            'requiredName' => array (
+                'rule' => 'notEmpty',
+                'required' => true,
+                'allowEmpty' => false,
+                'message' => 'Project name must be present.'
+            ),
+            'uniqueName' => array (
+                'rule'    => 'isUnique',
+                'message' => 'Project name has already been used.'
+            )
+        ),
+        'description'
     );
     
 }
