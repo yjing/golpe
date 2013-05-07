9c14bb5cc576b4fb0ad3aa79c9e3b6a3aba00c10
Stefano Susini <s.susini@gmail.com>
2013-05-07 15:35:27 +0100

    Projects and Teams management

--- app/Controller/RESTController.php
+++ app/Controller/RESTController.php
@@ -61,6 +61,7 @@ abstract class RESTController extends AppController {
         } else {
             return false;
         }
+        debug($this->_authorization);
         
         $user_role = $user['role'];
         $ret = false;
