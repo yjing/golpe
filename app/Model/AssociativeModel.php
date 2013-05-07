cb24f5a5faae71963c34c88b09469a7a4326caf0
Stefano Susini <s.susini@gmail.com>
2013-05-07 10:23:12 +0100

    Projects and Teams management

--- app/Model/AssociativeModel.php
+++ app/Model/AssociativeModel.php
@@ -126,7 +126,7 @@ abstract class AssociativeModel extends AppModel {
             $association_config['className'] . '.' . $association_config['foreignKey'] . ' = ' . $elem_id
         );
         if(isset($queryData['fields'])){
-            $queryData['fields']['DISTINCT'] = null;
+            $queryData['fields'][] = 'DISTINCT';
         } else {
             $queryData['fields'] = 'DISTINCT ' . $associated_model->alias . '.*';
         }
