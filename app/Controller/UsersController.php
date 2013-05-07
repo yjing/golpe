e46702cb94abaa8ee25872cc9fef6a02fd707944
Stefano Susini <s.susini@gmail.com>
2013-05-07 18:27:59 +0100

    Projects and Teams management

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -35,11 +35,11 @@ class UsersController extends RESTController {
         $data = $this->request->data;
         if($data) {
             // CHECK UNIQUE USERNAME
-            $this->_CheckUniqueUsername(Set::get($data, 'User.username'));
+//            $this->_CheckUniqueUsername(Set::get($data, 'User.username'));
             // REMOVE EVENTUAL CLIENT PROVIDED USER.ID
-            $data = Set::remove($this->request->data, 'User.id');
+//            $data = Set::remove($this->request->data, 'User.id');
             
-            $this->User->getDataSource()->begin();
+//            $this->User->getDataSource()->begin();
             
             $this->User->set($data);
             $this->User->validates();
