2c99aaad71597feedfcb508559c3ec500396b99e
Stefano Susini <s.susini@gmail.com>
2013-05-07 18:20:33 +0100

    Projects and Teams management

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -43,7 +43,7 @@ class UsersController extends RESTController {
             
             $this->User->set($data);
             $this->User->validates();
-            debug($this->User->validationErrors);
+            debug($this->User->validationErrors, true, false);
             debug($this->User->invalidFields());
             die();
             
