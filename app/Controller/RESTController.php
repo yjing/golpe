5a96b6395c2e17672fc8ad1bbfe19637ca056514
Stefano Susini <s.susini@gmail.com>
2013-05-08 10:08:38 +0100

    Projects and Teams management

--- app/Controller/RESTController.php
+++ app/Controller/RESTController.php
@@ -42,8 +42,9 @@ abstract class RESTController extends AppController {
         
         if(in_array($this->action, array('add', 'update'))) {
             if(isset($this->request->data)) {
-                $this->request->data = Set::remove($this->request->data, $this->modelClass . '.created');
-                $this->request->data = Set::remove($this->request->data, $this->modelClass . '.modified');
+                $this->request->data = Set::remove($this->request->data, '{n}.created');
+//                $this->request->data = Set::remove($this->request->data, $this->modelClass . '.created');
+//                $this->request->data = Set::remove($this->request->data, $this->modelClass . '.modified');
             }
             debug($this->request->data);
             die();
