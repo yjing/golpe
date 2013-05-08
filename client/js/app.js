ce64dc06eadfab9198706ec4d5fbd083d764b779
Stefano Susini <s.susini@gmail.com>
2013-05-08 11:13:08 +0100

    Client tuning

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,7 @@
 mscproject module
 it defines the client side application.
 */
-var app = angular.module('mscproject', [ 'ngResource', 'ngCookies', 'SSUtilities' ], function($routeProvider, $locationProvider) {
+var app = angular.module('mscproject', [ 'ngResource', 'SSUtilities' ], function($routeProvider, $locationProvider) {
     $routeProvider.when('/client/login', {
         templateUrl: '/client/partials/login.html',
         controller: "LoginCtrl"
