80f0f4c7850583b0649b7c91067e46cd000ec445
Stefano Susini <s.susini@gmail.com>
2013-05-08 11:23:29 +0100

    Client tuning

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,3 +1,28 @@
-function LoginCtrl($scope, $location, $http, auth) {
+function LoginCtrl($scope, $rootScope, $location, $resource, $http, auth) {
+    // TMP CREDENTIALS
+    $scope.username = "s.susini";
+    $scope.password = "30071980";
 
+    var User = $resource('/users/user');
+    $rootScope.user = User.get({}, function(data){
+        console.log(data);
+    });
+
+    // MENUS
+    $scope.mainmenu_open = "";
+    $scope.toggleMenu = function() {
+        if($scope.mainmenu_open.length == 0) {
+            $scope.mainmenu_open = "open";
+        } else {
+            $scope.mainmenu_open = "";
+        }
+    }
+    $scope.titlemenu_open = "";
+    $scope.toggleTitleMenu = function() {
+        if($scope.titlemenu_open.length == 0) {
+            $scope.titlemenu_open = "open";
+        } else {
+            $scope.titlemenu_open = "";
+        }
+    }
 }
