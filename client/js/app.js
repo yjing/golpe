f56320c85598dabf84564573a56b3a3b83fd0ef9
Stefano Susini <s.susini@gmail.com>
2013-05-09 17:33:07 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/app.js
+++ client/js/app.js
@@ -561,7 +561,7 @@ function GenericResource(resource, uri, def, config) {
                         }
                     }
 //                    if(callbacks.each != null) {
-                        console.log(data);
+                        console.log(data.length);
                         for(var i=0; i<data.length; i++) {
                             var ret = callbacks.each(data[i], headers);
                             if(ret != null) {
