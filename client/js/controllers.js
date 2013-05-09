770a52de358d9caa5eb6583658c04f50924c2be5
Stefano Susini <s.susini@gmail.com>
2013-05-09 08:25:15 +0100

    Client tuning

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -193,7 +193,7 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, Projects) {
         $scope.shownProject = index;
 
         $rootScope.busy(false);
-        $scope.project = Projects.get(p.Project.id);
+        $scope.project = Projects.get($scope.project.id);
         $scope.project.$then(function(){
             $rootScope.busy(false);
         });
