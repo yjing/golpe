7b43c125e3fd70d6033c771bb311eb1b8721906b
Stefano Susini <s.susini@gmail.com>
2013-05-09 15:37:15 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/app.js
+++ client/js/app.js
@@ -189,6 +189,9 @@ var app = angular.module('mscproject', [ 'ngResource' ], function($routeProvider
         }
     });
 
+    this.all = function(params, payload, callbacks) {
+        this.call('all', params, payload, callbacks);
+    }
     this.call = function(method, params, payload, callbacks) {
         return this.res[method](
             params,
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -153,7 +153,7 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, Projects, Projects2) 
 
     $rootScope.busy(false);
     $scope.main = function() {
-        var t = Projects2.call('all',{id:1},  {}, {
+        var t = Projects2.all({id:1},  {}, {
             each: function(data){
                 data.mode = 'partial';
             }
