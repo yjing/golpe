6c625bf60e8144abb9340ecd2aa1bfacb5231785
Stefano Susini <s.susini@gmail.com>
2013-05-09 19:36:01 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -213,7 +213,7 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, Projects) {
 
     $scope.editProject = function(){
         $scope.projects[$scope.p_index].undoEdit = {
-            'Project': $scope.projects[$scope.p_index].Project,
+            'Project': new $scope.projects[$scope.p_index].Project(),
             'mode': $scope.projects[$scope.p_index].mode
         };
         $scope.projects[$scope.p_index].mode = 'edit';
