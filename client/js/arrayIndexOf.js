09ea229a9151c648f8cf0219ced858ce12804e73
Stefano Susini <s.susini@gmail.com>
2013-05-10 23:09:17 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/arrayIndexOf.js
+++ client/js/arrayIndexOf.js
@@ -1,4 +1,4 @@
-console.log(!Array.prototype.indexOf);
+console.log([2, 4, 5].indexOf(4));
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
         "use strict";
