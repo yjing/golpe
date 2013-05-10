b88f5123cf6be595d298893bd356d3bac65ab0ff
Stefano Susini <s.susini@gmail.com>
2013-05-10 18:49:17 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/app.js
+++ client/js/app.js
@@ -345,7 +345,7 @@ var app = angular.module('mscproject', [ 'ngResource' ], function($routeProvider
     this.cancelEditProject = function(index) {
         if(this.projects[index].status == STATUS_NEW) {
             this.projects.splice(index, 1);
-            this.activate(0);
+            this.activate(100);
         } else {
             this.projects[index] = this.projects[index].old;
             this.projects[index].mode = MODE_NORMAL;
