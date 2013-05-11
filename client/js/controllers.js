0c90dd712a9f41adaff449a66879ea72baf7b90c
Stefano Susini <s.susini@gmail.com>
2013-05-11 17:16:32 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -217,28 +217,29 @@ function ProjectsCtrl($scope, $rootScope, $location, $resource, auth, Projects, 
                 function(d, h){
                     $scope.selected_project = id;
 
-                    var test = "Project.Team.Student";
-                    console.log(getIdTree(d, "Project.Team.Student", "teams.users"));
-
-                    var newMenu = [];
+                    var newProjectTree = getIdTree(d, "Project.Team.Student", "teams.users");
                     var index = $scope.findIndexById(id);
-                    if(angular.isUndefined(index)) {
-                        $scope.menu.push({"id":id});
-                        index = $scope.menu.length - 1;
-                    }
-                    if(angular.isDefined(d['Project']['Team']) && angular.isArray(d['Project']['Team'])) {
-                        $scope.menu[index].teams = [];
-                        for(var i=0; i<d['Project']['Team'].length; i++) {
-                            $scope.menu[index].teams.push({"id" : d['Project']['Team'][i].id});
-                            if (angular.isArray(d['Project']['Team'][i]['Student'])) {
-                                $scope.menu[index].teams[i].users = [];
-                                for(var j=0; j<d['Project']['Team'][i]['Student'].length; j++) {
-                                    $scope.menu[index].teams[i].users.push({ "id": d['Project']['Team'][i]['Student'][j].id });
-                                }
-                            }
-                        }
-                        console.log($scope.menu);
-                    }
+                    $scope.menu[index] = newProjectTree;
+
+//                    var newMenu = [];
+//                    var index = $scope.findIndexById(id);
+//                    if(angular.isUndefined(index)) {
+//                        $scope.menu.push({"id":id});
+//                        index = $scope.menu.length - 1;
+//                    }
+//                    if(angular.isDefined(d['Project']['Team']) && angular.isArray(d['Project']['Team'])) {
+//                        $scope.menu[index].teams = [];
+//                        for(var i=0; i<d['Project']['Team'].length; i++) {
+//                            $scope.menu[index].teams.push({"id" : d['Project']['Team'][i].id});
+//                            if (angular.isArray(d['Project']['Team'][i]['Student'])) {
+//                                $scope.menu[index].teams[i].users = [];
+//                                for(var j=0; j<d['Project']['Team'][i]['Student'].length; j++) {
+//                                    $scope.menu[index].teams[i].users.push({ "id": d['Project']['Team'][i]['Student'][j].id });
+//                                }
+//                            }
+//                        }
+//                        console.log($scope.menu);
+//                    }
                 },
                 function(e) {
                     $rootScope.handleError(e);
