bbc7ee07e2719d90182d1187cb667d061de07bac
Stefano Susini <s.susini@gmail.com>
2013-05-12 08:43:47 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -326,15 +326,19 @@ function ProjectsCtrl($scope, $rootScope, $location, $resource, auth, Projects, 
     }
 
     $scope.newTeam = function(save) {
-        if(angular.isNumber(save)) {
-            $scope.new_team = {
-                'project_id': save,
-                'name':''
-            };
-            $scope.selected_team = -1;
-            console.log($scope.new_project);
-        } else {
+        if(angular.isDefined(save)) {
+            if(angular.isNumber(save)) {
+                $scope.new_team = {
+                    'project_id': save,
+                    'name':''
+                };
+                $scope.selected_team = -1;
+                console.log($scope.new_project);
+            } else {
 
+            }
+        } else {
+            return $scope.new_team != null;
         }
     }
     $scope.cancelNewTeam = function() {
