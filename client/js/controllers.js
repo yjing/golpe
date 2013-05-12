26329c5838c1109856e3e04038a82df24b2a6d69
Stefano Susini <s.susini@gmail.com>
2013-05-12 11:31:24 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -398,6 +398,7 @@ function ProjectsCtrl($scope, $rootScope, $location, $resource, auth, Projects, 
     }
 
     $scope.removeMember = function(confirm, u_id, t_id){
+        console.log(DBService.m.users[u.id]);
         if(angular.isDefined(DBService.d.teams[t_id]) && angular.isDefined(DBService.d.users[u_id])){
             if(confirm == true) {
                 ProjectsService.removeMember(id,
