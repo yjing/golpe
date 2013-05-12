11bfa60e7130fa628cbd390dd82a7f52426e0659
Stefano Susini <s.susini@gmail.com>
2013-05-12 15:41:14 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -508,7 +508,7 @@ function StudentCtrl($scope, $rootScope, $location, auth, BusyService, ALService
             // SUCCESS
             function(data, handlers){
                 $scope.menu = DBService.d.als;
-                $scope.PIPPO(4);
+                $scope.$electAL(4);
             },
             // ERROR
             function(error){
@@ -533,7 +533,7 @@ function StudentCtrl($scope, $rootScope, $location, auth, BusyService, ALService
         $scope.main();
     }
 
-    $scope.PIPPO = function(id) {
+    $scope.$electAL = function(id) {
         console.log(id);
         if(id < 0){
             $scope.selected_al = -1;
