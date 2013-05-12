b32cc411fcff49964824cd90fab700a179856515
Stefano Susini <s.susini@gmail.com>
2013-05-12 13:28:22 +0100

    Merge branch 'master' of https://github.com/stefanosusini/golpe

--- client/js/app.js
+++ client/js/app.js
@@ -642,7 +642,6 @@ var app = angular.module('mscproject', [ 'ngResource' ], function($routeProvider
             { "mode": config.mode },
             function(d, h) {
                 BusyService.busy(false);
-                console.log(d[0]['ActivityLog']['id']);
                 _THIS.insertALs(d);
 
                 // CALLBACKS
@@ -663,6 +662,7 @@ var app = angular.module('mscproject', [ 'ngResource' ], function($routeProvider
     }
 
     this.insertALs = function(d) {
+        console.log(d[0]['ActivityLog']['id']);
         if(angular.isArray(d)) {
             for(var i=0; i< d.length; i++) {
                 this.insertAL(d[i]['ActivityLog']);
