ef565a4268fa2874509846c3de4d10306479e095
Stefano Susini <s.susini@gmail.com>
2013-05-13 07:46:27 +0100

    Web Client

--- client/js/app.js
+++ client/js/app.js
@@ -710,7 +710,8 @@ var app = angular.module('mscproject', [ 'ngResource' ], function($routeProvider
             var params = {'id':id};
 
             BusyService.busy(true);
-            var al = this.ALs.save(
+            var al
+                = this.ALs.save(
                 params,
                 data,
                 function(d, h) {
