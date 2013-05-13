91f735a1411ac5ac34c5990f63cc43e29b7e1d54
Stefano Susini <s.susini@gmail.com>
2013-05-13 12:20:39 +0100

    Web Client

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -625,6 +625,7 @@ function StudentCtrl($scope, $rootScope, $location, auth, BusyService, ALService
                         for(var i=0; i<DBService.d.als.length; i++) {
                             menu.push(DBService.d.als[i]);
                         }
+                        $scope.reload();
                         $scope.menu = menu;
                     },
                     // ERROR
