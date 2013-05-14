2894d5861babb45ba1ed71e7c158e7ede1b598e3
Stefano <s.susini@gmail.com>
2013-05-14 19:01:48 +0200

    Update tmp.php

--- app/Model/Behavior/tmp.php
+++ app/Model/Behavior/tmp.php
@@ -54,6 +54,16 @@ function beforeFind ( $query )
 
 
 
-
+array(
+    'conditions' => array('Model.field' => $thisValue), //array of conditions
+    'recursive' => 1, //int
+    'fields' => array('Model.field1', 'DISTINCT Model.field2'), //array of field names
+    'order' => array('Model.created', 'Model.field3 DESC'), //string or array defining order
+    'group' => array('Model.field'), //fields to GROUP BY
+    'limit' => n, //int
+    'page' => n, //int
+    'offset' => n, //int
+    'callbacks' => true //other possible values are false, 'before', 'after'
+)
 
 
