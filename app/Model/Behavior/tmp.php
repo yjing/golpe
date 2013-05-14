3b58e1c579983abdacc2de535b64cc9bb116eea1
Stefano <s.susini@gmail.com>
2013-05-14 16:36:38 +0200

    Update tmp.php

--- app/Model/Behavior/tmp.php
+++ app/Model/Behavior/tmp.php
@@ -1,3 +1,23 @@
 <?php
 
-function 
+function beforeFind ( $query )
+  $user <- getLoggedUser();
+  
+  if ( $user IS SUPER_USER ) {
+    return $query; 
+    // ACCESS IS GRANTED  TO ALL DATA
+    // NO FURTHER CONDITION IS ADDED TO THE $query
+  }
+  
+  // GENERATE THE CONDITIONS TO ADD TO THE USER'S CONDITIONS
+  $raw_conditions <- getConditionsConfig ( 'CONFIG_FILE' );
+  $prepared_conditions <- parseAndPrepareConditions ( $raw_conditions );
+  $query->conditions += $prepared_conditions;
+  
+  // GENERATE JOINS TO DATA ACCESS RELATED ENTITIES
+  $join_config <- getJoinConfig ( 'CONFIG_FILE' );
+  $prepared_joins <- generateJoins ( 'CONFIG_FILE' );
+  $query->joins += $prepared_joins;
+  
+  return $query;
+}
