574eaf6ea1e84e68f98afe3731c0b669ff662677
Stefano Susini <s.susini@gmail.com>
2013-05-18 16:47:10 +0100

    test branch

--- client/js/app.js
+++ client/js/app.js
@@ -246,7 +246,7 @@ var app = angular.module('mscproject', [ 'ngResource', 'SSDB' ], function($route
 })
 .service('UsersService', function($rootScope, $resource, BusyService, DBService, SSDBService){
 
-        SSDBService.addFKey ('test');
+//        SSDBService.addFKey ('test');
         SSDBService.addFKey ('test', null);
 
         SSDBService.createTable("test");
