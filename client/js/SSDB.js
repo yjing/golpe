a3cb1d50a97fad4a10f2b95b7950f255bf73003e
Stefano Susini <s.susini@gmail.com>
2013-05-19 11:52:44 +0100

    test branch

--- client/js/SSDB.js
+++ client/js/SSDB.js
@@ -132,6 +132,7 @@ function Table(name, pkey, blgTo, hsMany) {
     this.getData = function(recursive){
         var res = toList(data);
         console.log(res);
+        console.log(belongsTo);
         if(recursive) {
             angular.forEach(belongsTo, function(v,k){
                 console.log('belongsTo: ' + k);
