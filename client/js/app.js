d71ef8917aeb97c0d74e0fc964f79a92a657d02d
Stefano Susini <s.susini@gmail.com>
2013-05-20 14:37:23 +0100

    test branch

--- client/js/app.js
+++ client/js/app.js
@@ -259,7 +259,7 @@ var app = angular.module('mscproject', [ 'ngResource', 'SSDB' ],function ($route
         }
 
     })
-    .service('ProjectsService', function(database){
+    .service('ProjectsService', function($resource, database){
         this.DATA_KEY = 'Project';
         this.TABLE = 'Projects';
         this.PKEY = 'id';
