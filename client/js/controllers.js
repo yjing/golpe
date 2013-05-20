6a33710a5b5fc26747997cefd5944867b985c9ab
Stefano Susini <s.susini@gmail.com>
2013-05-20 17:17:01 +0100

    test branch

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -338,6 +338,9 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, Projects, ProjectsSer
                     var proj = database.get(ProjectsService.TABLE, id, 3);
                     var index = $scope.meta[id].index;
                     $scope.projectsData[index] = proj;
+
+                    console.log($scope.projectsData);
+
                 },
                 function(e) {
                     $rootScope.handleError(e);
