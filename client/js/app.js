22b09f7c71e7c8849d161ec60a5b584769a8c000
Stefano Susini <s.susini@gmail.com>
2013-05-20 17:23:50 +0100

    test branch

--- client/js/app.js
+++ client/js/app.js
@@ -324,7 +324,7 @@ var app = angular.module('mscproject', [ 'ngResource', 'SSDB' ],function ($route
                     function (d, h) {
                         BusyService.busy(false);
 
-                        console.log(proj);
+                        console.log(d);
                         // ADD METADATA
                         _THIS.insertProject(proj[_THIS.DATA_KEY]);
 
