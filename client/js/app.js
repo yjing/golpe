dbfd85cd7f3701a20420fded1fb7a1e871a747c0
Stefano Susini <s.susini@gmail.com>
2013-05-21 12:28:34 +0100

    test branch

--- client/js/app.js
+++ client/js/app.js
@@ -466,6 +466,8 @@ var app = angular.module('mscproject', [ 'ngResource', 'SSDB' ],function ($route
                 function(d, h) {
                     BusyService.busy(false);
 
+                    _THIS.insertTeam(d[_THIS.DATA_KEY]);
+
                     // CALLBACKS
                     if (angular.isDefined(success)) {
                         success(d, h);
