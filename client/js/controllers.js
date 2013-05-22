99899cb64463c5b840c8c2fe621a154f48982c62
Stefano Susini <s.susini@gmail.com>
2013-05-22 09:14:40 +0100

    test branch

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -293,6 +293,8 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, BusyService, Projects
     // PROJECTS RELATED FUNCTIONS
     $scope.selectElem = function (index) {
         $scope.selected_elem = $scope.elements[index];
+        console.log($scope.elements[index]);
+        console.log($scope.selected_elem);
         $scope.selected_elem_id = $scope.selecred_elem[ProjectsService.PKEY];
     };
     $scope.isSelectedElem = function (id) {
