5c0a8dd0b76981b7a0969b964cbbc5353b1594cc
Stefano Susini <s.susini@gmail.com>
2013-05-22 13:45:07 +0100

    test branch

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -465,7 +465,7 @@ function ProjectsCtrl($scope, $rootScope, $location, auth, BusyService, Projects
         _THIS.setTeamMeta(id, { mode: MODE_EDIT });
     };
     $scope.isEditTeam = function (id) {
-        console.log(database.select("TeamsMeta", [], 0));
+        console.log(database.get("TeamsMeta", id, 0));
         return _THIS.getTeamMeta(id, MODE_KEY) == MODE_EDIT;
     };
 
