d58b4a1be5bfa14dd34ad8d4d827d43b82aa72f6
Stefano Susini <s.susini@gmail.com>
2013-05-22 17:31:50 +0100

    test branch

--- client/js/app.js
+++ client/js/app.js
@@ -507,7 +507,7 @@ var app = angular.module('mscproject', [ 'ngResource', 'SSDB' ],function ($route
                 function (d, h) {
                     BusyService.busy(false);
 
-                    var user = database.get('Users', u_id, 0);
+                    var user = database.get('Users', u_id, 1);
                     console.log(user);
                     delete user[this.TEAM_FKEY];
 
