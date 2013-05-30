3aee0f924d9fedc800406089b29c8c9f23e65936
Stefano Susini <s.susini@gmail.com>
2013-05-30 17:04:33 +0100

    New Client

--- client/js/app.js
+++ client/js/app.js
@@ -3,11 +3,11 @@
  it defines the client side application.
  */
 var app = angular.module('mscproject', [ 'ngResource', 'ui.bootstrap'],function ($routeProvider, $locationProvider) {
-//    $routeProvider.when('/client/login', {
-//        templateUrl:'/client/partials/login.html',
-//        controller:"LoginCtrl"
-//    });
-//    $routeProvider.otherwise({redirectTo:'/client/login'});
+    $routeProvider.when('/client/login', {
+        templateUrl:'/client/parts/login.html',
+        controller:"LoginCtrl"
+    });
+    $routeProvider.otherwise({redirectTo:'/client/login'});
 
     // configure html5 to get links working
     // If you don't do this, you URLs will be base.com/#/home rather than base.com/home
@@ -20,8 +20,4 @@ function supports_html5_storage() {
     } catch (e) {
         return false;
     }
-}
-
-function TestCtrl($scope){
-    $scope.isCollapsed = false;
 }
