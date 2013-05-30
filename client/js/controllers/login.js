c594d0ba48a3e025e618894ea65443ef2e9038ae
Stefano Susini <s.susini@gmail.com>
2013-05-30 19:45:19 +0100

    New Client

--- client/js/controllers/login.js
+++ client/js/controllers/login.js
@@ -1,16 +1,27 @@
-function LoginCtrl($scope, $rootScope, auth){
+function LoginCtrl($scope, $rootScope, $location, auth){
+
+    auth.user(
+        function (user){
+            if(user != null) {
+                $scope.redirectUser(user);
+            }
+        }
+    );
 
     $scope.username = "s.susini";
     $scope.password = "30071980";
 
-    auth.login($scope.username, $scope.password,
-        function (user){
-            console.log(user);
-            auth.logout (function (user){
-                console.log(user);
-            });
-        }
-    );
+    $scope.login = function(){
+        auth.login($scope.username, $scope.password,
+            function(user){
+                if(user != null) {
+                    $scope.redirectUser(user);
+                } else {
+                    $scope.login_message = "Error";
+                }
+            }
+        );
+    }
 
     // TOP BAR
     $rootScope.top_bar = {
