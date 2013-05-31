868108353313f963e59f001705d444fc737db030
Stefano Susini <s.susini@gmail.com>
2013-05-31 16:47:56 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -17,7 +17,7 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
                     $scope.data = d;
                     if (angular.isDefined($scope.selected_al_id)
                         && $scope.selected_al_id != null
-                        && !angular.isString($scope.selected_al_id)) {
+                        ) {
 
                         als.get(
                             $scope.selected_al_id,
