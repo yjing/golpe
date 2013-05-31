4eff3188cb530d43dc48ab501196b46dd60f53cd
Stefano Susini <s.susini@gmail.com>
2013-05-31 15:47:02 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -32,7 +32,7 @@ function StudentCtrl($scope, $rootScope, $location, auth, als, database){
     $rootScope.top_bar = {
         page_title: 'Activity Logs',
         main_menu_items: [
-            { type: 'item', label: 'Add Activity Log', icon: 'icon-plus', func: $rootScope.logout },
+            { type: 'item', label: 'New Activity Log', icon: 'icon-plus', func: $rootScope.logout },
             { type: 'divider' },
             { type: 'item', label: 'Logout', icon: 'icon-lock', func: $rootScope.logout },
             { type: 'item', label: 'Help', icon: 'icon-question-sign', func: $rootScope.help },
