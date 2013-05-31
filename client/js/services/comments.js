ea6d62b7b6b310a147866bf158f61b8c245b90e8
Stefano Susini <s.susini@gmail.com>
2013-05-31 13:11:35 +0100

    New Client

--- client/js/services/comments.js
+++ client/js/services/comments.js
@@ -15,45 +15,6 @@ app.factory('comments_db', function(database){
             return database.insert('comments', comment['id'], comment);
         }
     }
-}).service('resources', function($resource){
-
-    this.Users = $resource('/projects/:id', { id:'@id' }, {
-        all:{
-            method:'GET',
-            isArray:true
-        },
-        load:{
-            method:'GET',
-            isArray:false
-        },
-        user:{
-            method:'GET',
-            url:'/users/user'
-        },
-        login:{
-            method:'POST',
-            url:'/users/login',
-            headers:{'Content-Type':'application/x-www-form-urlencoded'}
-        },
-        logout:{
-            method:'GET',
-            url:'/users/logout'
-        }
-    });
-
-    this.Als = $resource('/activity_logs/:id', { id:'@id' }, {
-        all:{
-            method:'GET',
-            isArray:true
-        },
-        load:{
-            method:'GET',
-            isArray:false
-        },
-        modes:{
-            method:'GET',
-            url:'/activity_logs/modes'
-        }
-    });
+}).service('comments', function(resources){
 
 });
