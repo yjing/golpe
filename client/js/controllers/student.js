680798aca34cfd397344f530b146052c998071ca
Stefano Susini <s.susini@gmail.com>
2013-05-31 16:46:00 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -1,24 +1,27 @@
-function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, database){
+function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, database) {
 
     $scope.data = null;
     $scope.selected_al_id = $routeParams.id;
     $scope.selected_al = null;
 
     auth.user(
-        function (user){
-            if(user == null) {
+        function (user) {
+            if (user == null) {
                 $location.url('/client/login');
                 return;
             }
             als.all(
-                false,                          // RELOAD
-                'all',                          // MODE
-                function(d, h) {                // SUCCESS
+                false, // RELOAD
+                'all', // MODE
+                function (d, h) {                // SUCCESS
                     $scope.data = d;
-                    if (angular.isDefined($scope.selected_al_id) && $scope.selected_al_id != null && angular.isNumber($scope.selected_al_id)) {
+                    if (angular.isDefined($scope.selected_al_id)
+                        && $scope.selected_al_id != null
+                        && angular.isNumber($scope.selected_al_id)) {
+                        console.log('loading');
                         als.get(
                             $scope.selected_al_id,
-                            function(d, h) {    // SUCCESS
+                            function (d, h) {    // SUCCESS
                                 $scope.selected_al = d;
                             }
                         );
@@ -30,7 +33,7 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
 
     $scope.go = function (id) {
         var url = '/client/student';
-        if(angular.isDefined(id)) {
+        if (angular.isDefined(id)) {
             url += '/' + id;
         }
         $location.url(url);
@@ -40,7 +43,7 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
         als.all(
             true,
             'all',
-            function(d, h) {    // SUCCESS
+            function (d, h) {    // SUCCESS
                 $scope.data = d;
             }
         );
@@ -48,13 +51,13 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
 
     // TOP BAR
     $rootScope.top_bar = {
-        page_title: 'Activity Logs',
-        main_menu_items: [
-            { type: 'item', label: 'New Activity Log', icon: 'icon-plus', func: $rootScope.logout },
-            { type: 'divider' },
-            { type: 'item', label: 'Logout', icon: 'icon-lock', func: $rootScope.logout },
-            { type: 'item', label: 'Help', icon: 'icon-question-sign', func: $rootScope.help },
-            { type: 'item', label: 'Info', icon: 'icon-info-sign', func: $rootScope.info }
+        page_title:'Activity Logs',
+        main_menu_items:[
+            { type:'item', label:'New Activity Log', icon:'icon-plus', func:$rootScope.logout },
+            { type:'divider' },
+            { type:'item', label:'Logout', icon:'icon-lock', func:$rootScope.logout },
+            { type:'item', label:'Help', icon:'icon-question-sign', func:$rootScope.help },
+            { type:'item', label:'Info', icon:'icon-info-sign', func:$rootScope.info }
         ]
     };
 }
