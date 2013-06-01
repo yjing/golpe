396a2b0061cad8542ec87450ba7ac32fa0c97d69
Stefano Susini <s.susini@gmail.com>
2013-06-01 19:42:11 +0100

    New Client

--- client/js/app.js
+++ client/js/app.js
@@ -69,7 +69,7 @@ var app = angular.module('mscproject', [ 'ngResource', 'ui.bootstrap'],function 
         }
 
         $rootScope.error = function (err_data) {
-            console.log("TEST1");
+            console.log(err_data);
             if (err_data.status == 401 && err_data.message == 'NO-LOGGED') {
                 console.log("TEST2");
                 $location.url('/client/login');
