0d2d97b82952f36df5eff7f1765f8d85837f9425
Stefano <s.susini@gmail.com>
2013-06-01 12:23:52 +0200

    Update CommentsController.php

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -51,7 +51,9 @@ class CommentsController extends RESTController {
         if ($this->request->data) {
             $data = Set::remove($this->request->data, 'Comment.id');
 
-            if($this->Comment->save($data)) {
+            $res = $this->Comment->save($data);
+            debug("saved??");
+            if($res) {
                 $this->_setResponseJSON( $this->getDafaultFormattedComment($this->Comment->id) );
             } else {
                 if(count($this->Comment->validationErrors) > 0) {
