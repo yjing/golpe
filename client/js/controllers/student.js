6d0651ee7c3098551c7980f35d3762751e70bcda
Stefano Susini <s.susini@gmail.com>
2013-06-01 21:57:29 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -1,16 +1,17 @@
 function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, database) {
     // TOP BAR
-    $rootScope.top_bar = {
-        page_title:'Activity Logs',
-        main_menu_items:[
-            { type:'item', label:'New Activity Log', icon:'icon-plus', func:$rootScope.logout },
-            { type:'divider' },
-            { type:'item', label:'Logout', icon:'icon-lock', func:$rootScope.logout },
-            { type:'item', label:'Help', icon:'icon-question-sign', func:$rootScope.help },
-            { type:'item', label:'Info', icon:'icon-info-sign', func:$rootScope.info }
-        ]
-    };
     $scope.setupTopBar = function () {
+        $rootScope.top_bar = {
+            page_title:'Activity Logs',
+            main_menu_items:[
+                { type:'item', label:'New Activity Log', icon:'icon-plus', func:$rootScope.logout },
+                { type:'divider' },
+                { type:'item', label:'Logout', icon:'icon-lock', func:$rootScope.logout },
+                { type:'item', label:'Help', icon:'icon-question-sign', func:$rootScope.help },
+                { type:'item', label:'Info', icon:'icon-info-sign', func:$rootScope.info }
+            ]
+        };
+
         if($scope.selected_al_id != null && $rootScope.isMobile) {
             $rootScope.top_bar.page_title = "Activity Log";
             $rootScope.top_bar.back_button = {
