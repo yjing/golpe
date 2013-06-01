ed8d05aae48e3b9531e564adf8d9a9b14f502327
Stefano Susini <s.susini@gmail.com>
2013-06-01 21:08:08 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -21,19 +21,16 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
 
                 if(mode == $rootScope.mode) {
                     $rootScope.top_bar.page_title = manuLabel;
+
                 } else {
-                    var test = function(elem){
-                        console.log(elem);
+                    var modeSelector = function(elem){
+                        $rootScope.mode = elem.func.mode;
+                        $scope.setupTopBar();
                     };
-                    test.mode = mode;
+                    modeSelector.mode = mode;
                     $rootScope.top_bar.title_menu.push({
                         label: manuLabel,
-                        func: test
-//                            function(elem) {
-//                            console.log(elem);
-//                            $rootScope.mode = mode;
-//                            $scope.setupTopBar();
-//                        }
+                        func: modeSelector
                     });
                 }
             }
