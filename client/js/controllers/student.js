61be688534189aa1cdea1c7bf865fdb11d2d753b
Stefano Susini <s.susini@gmail.com>
2013-06-02 07:31:51 +0100

    New Client

--- client/js/controllers/student.js
+++ client/js/controllers/student.js
@@ -215,7 +215,7 @@ function StudentCtrl($scope, $rootScope, $routeParams, $location, auth, als, dat
                         function (datum, h) {    // SUCCESS
                             $scope.selected_al = database.select('als', [ {field:'id',value:$scope.selected_al_id} ], 2)[0];
                             $scope.add_comment = false;
-                            document.formc.reset();
+                            angular.element('formc').reset();
                         }
                     );
                 } else {
