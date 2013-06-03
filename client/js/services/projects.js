7a018419a4ecea77e65ca66262d972f14f595a3f
Stefano Susini <s.susini@gmail.com>
2013-06-03 18:01:55 +0100

    New Client

--- client/js/services/projects.js
+++ client/js/services/projects.js
@@ -9,8 +9,8 @@ app.factory('projects_db',function (database, teams_db) {
         };
         this.insertProject = function (project) {
             if(angular.isDefined(project['Team'])) {
-                console.log(project['Team']);
                 if(angular.isArray(project['team']) && project['Team'].length > 0) {
+                    console.log(project['Team']);
                     teams_db.insertTeams(project['Team']);
                 }
                 project.status = 'complete';
