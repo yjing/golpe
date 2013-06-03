84e9c321d5d150820027ef57a1ce13f5555bfd4d
Stefano Susini <s.susini@gmail.com>
2013-06-03 17:03:39 +0100

    New Client

--- client/js/controllers/projectsCtrl.js
+++ client/js/controllers/projectsCtrl.js
@@ -1,4 +1,4 @@
-function ProjectsCtrl($scope, $rootScope, $routeParams, database, projects){
+function ProjectsCtrl($scope, $rootScope, $routeParams, $location, database, projects){
     // TOP BAR
     $scope.setupTopBar = function () {
         $rootScope.top_bar = {
