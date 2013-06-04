d9684d0ae9972af80fbc6abb299a06fb138ac88e
Stefano Susini <s.susini@gmail.com>
2013-06-04 09:25:19 +0100

    New Client

--- client/js/controllers/projectsCtrl.js
+++ client/js/controllers/projectsCtrl.js
@@ -106,4 +106,35 @@ function ProjectsCtrl($scope, $rootScope, $routeParams, $location, $dialog, auth
         return $scope.selected_t_id == id ? 'active' : '';
     };
 
+    // DIALOGS AND MESSAGES
+    var t = '<div class="modal-header">'+
+        '<h3>This is the title</h3>'+
+        '</div>'+
+        '<div class="modal-body">'+
+        '<p>Enter a value to pass to <code>close</code> as the result: <input ng-model="result" /></p>'+
+        '</div>'+
+        '<div class="modal-footer">'+
+        '<button ng-click="close(result)" class="btn btn-primary" >Close</button>'+
+        '</div>';
+
+    $scope.opts = {
+        backdrop: true,
+        keyboard: true,
+        backdropClick: true,
+        template:  t, // OR: templateUrl: 'path/to/view.html',
+        controller: 'TestDialogController'
+    };
+
+    $scope.openMessageBox = function(){
+        var title = 'This is a message box';
+        var msg = 'This is the content of the message box';
+        var btns = [{result:'cancel', label: 'Cancel'}, {result:'ok', label: 'OK', cssClass: 'btn-primary'}];
+
+        $dialog.messageBox(title, msg, btns)
+            .open()
+            .then(function(result){
+                alert('dialog closed with result: ' + result);
+            });
+    };
+
 }
