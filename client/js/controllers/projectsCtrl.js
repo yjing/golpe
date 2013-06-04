c72bc86fea580ccaedc44581f9201b76b5a28978
Stefano Susini <s.susini@gmail.com>
2013-06-04 09:00:35 +0100

    New Client

--- client/js/controllers/projectsCtrl.js
+++ client/js/controllers/projectsCtrl.js
@@ -30,6 +30,7 @@ function ProjectsCtrl($scope, $rootScope, $routeParams, $location, auth, databas
     $scope.$watch('add_member', function(){
         if($scope.add_member) {
             $scope.member_list = $scope.filterTeamedStudents(database.select('users', [ {field:'role',value:'STUDENT'} ], 1));
+            console.log("HERE");
         } else {
             $scope.member_list = null;
         }
