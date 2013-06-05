bc6c8333b2dd6fc4a5de3bd313fbf66f072d5913
Stefano Susini <s.susini@gmail.com>
2013-06-05 09:53:39 +0100

    New Client

--- client/js/controllers/usersCtrl.js
+++ client/js/controllers/usersCtrl.js
@@ -66,7 +66,7 @@ function UsersCtrl($scope, $rootScope, $routeParams, $location, $dialog, auth, d
         users.save(
             $scope.selected_u,
             function(d, h) {    // SUCCESS
-                $scope.selected_u = database.select('users', [ {field:'id',value:$scope.selected_u_id} ], 3)[0];
+                $scope.unEditSelectedU();
             }
         );
     };
