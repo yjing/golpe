5e349cdb14e4765a6c2fe3f77f95d6f3e18ff0a0
Stefano Susini <s.susini@gmail.com>
2013-06-05 15:11:40 +0100

    Projects and Teams management

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -22,7 +22,8 @@ class UsersController extends RESTController {
             );
         }
         
-        $result = $this->getDafaultFormattedUsers($conditions);
+//        $result = $this->getDafaultFormattedUsers($conditions);
+        $result = $this->getDafaultFormattedUsers();
         $this->_setResponseJSON($result);
     }
 
