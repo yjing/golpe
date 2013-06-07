3259ac28e21a407bf6072e53243c47129280d66b
Stefano Susini <s.susini@gmail.com>
2013-06-07 08:31:15 +0100

    New Client

--- client/js/controllers/usersCtrl.js
+++ client/js/controllers/usersCtrl.js
@@ -46,9 +46,9 @@ function UsersCtrl($scope, $rootScope, $routeParams, $location, $dialog, auth, d
             reload,               // RELOAD
             function(d, h) {     // SUCCESS
                 $scope.data = database.select('users', [], 3);
-                $scope.supervisors = database.select('users', [ {field:'role', value:'SUPERVISOR'} ], 1);
-                console.log($scope.supervisors);
-                console.log(database.select('users', [ ], 1));
+//                $scope.supervisors = database.select('users', [ {field:'role', value:'SUPERVISOR'} ], 1);
+//                console.log($scope.supervisors);
+//                console.log(database.select('users', [ ], 1));
             }
         );
     };
